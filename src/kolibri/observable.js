5c0cc399b400c2dea6317736f0d30a7d58e8d45e
Dierk Koenig <dierk.koenig@canoo.com>
2021-11-19 14:07:33 +0100

    observable incl. tests, todo: jsdoc.

---
+++ src/kolibri/observable.js
@@ -0,0 +1,51 @@
+// todo jsdoc
+
+import "./array.js"
+
+export {Observable, ObservableList}
+
+const Observable = value => {
+    const listeners = [];
+    return {
+        onChange: callback => {
+            listeners.push(callback);
+            callback(value, value);
+        },
+        getValue: ()       => value,
+        setValue: newValue => {
+            if (value === newValue) return;
+            const oldValue = value;
+            value = newValue;
+            listeners.forEach(callback => {
+                if (value === newValue) { // pre-ordered listeners might have changed this and thus the callback no longer applies
+                    callback(value, oldValue)
+                }
+            });
+        }
+    }
+};
+
+
+const ObservableList = list => {
+    const addListeners = [];
+    const delListeners = [];
+    const removeAddListener    = addListener => addListeners.removeItem(addListener);
+    const removeDeleteListener = delListener => delListeners.removeItem(delListener);
+    return {
+        onAdd: listener => addListeners.push(listener),
+        onDel: listener => delListeners.push(listener),
+        add: item => {
+            list.push(item);
+            addListeners.forEach( listener => listener(item))
+        },
+        del: item => {
+            list.removeItem(item);
+            const safeIterate = [...delListeners]; // shallow copy as we might change listeners array while iterating
+            safeIterate.forEach( (listener, index) => listener(item, () => removeDeleteListener(listener) ));
+        },
+        removeAddListener,
+        removeDeleteListener,
+        count:   ()   => list.length,
+        countIf: pred => list.reduce( (sum, item) => pred(item) ? sum + 1 : sum, 0)
+    }
+};
