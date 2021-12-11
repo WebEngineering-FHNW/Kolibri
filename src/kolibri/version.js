8d298ff0f0abf467aef2e6f533ec2e54d175e04d
Dierk Koenig <dierk.koenig@canoo.com>
2021-12-11 22:28:39 +0100

    version bump since we introduced new public functions in array.js

--- src/kolibri/version.js
+++ src/kolibri/version.js
@@ -1,7 +1,7 @@
 export { release, dateStamp, versionInfo }
 
-const release       = "0.1.27";
+const release       = "0.1.28";
 
-const dateStamp     = "2021-12-05 T 18:41:06 MEZ";
+const dateStamp     = "2021-12-11 T 22:26:28 MEZ";
 
 const versionInfo   = release + " at " + dateStamp;
