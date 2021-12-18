14f144e4321db605034224eabcb77ff1c78edb98
Dierk Koenig <dierk.koenig@canoo.com>
2021-12-18 23:00:08 +0100

    housekeeping

--- src/kolibri/version.js
+++ src/kolibri/version.js
@@ -1,7 +1,7 @@
 export { release, dateStamp, versionInfo }
 
-const release       = "0.1.30";
+const release       = "0.1.31";
 
-const dateStamp     = "2021-12-18 T 21:43:26 MEZ";
+const dateStamp     = "2021-12-18 T 22:59:43 MEZ";
 
 const versionInfo   = release + " at " + dateStamp;
