6eaaa58e3100c7174ee2b52a7707750593f48bec
Dierk Koenig <dierk.koenig@canoo.com>
2022-01-02 00:55:53 +0100

    Math.random() does not always create the same number of digits.

--- src/kolibri/version.js
+++ src/kolibri/version.js
@@ -1,14 +1,15 @@
 export { release, dateStamp, versionInfo, clientId }
 
-const release       = "0.1.34";
+const release     = "0.1.34";
 
-const dateStamp     = "2022-01-02 T 00:22:51 MEZ";
+const dateStamp   = "2022-01-02 T 00:22:51 MEZ";
 
-const versionInfo   = release + " at " + dateStamp;
+const versionInfo = release + " at " + dateStamp;
 
+const stamp       = () => Math.random().toString(36).slice(2).padEnd(11,"X").slice(0,11);
 /**
  * An constant random string of 22 lowercase characters/digits, probability: 1 of 36 ** 22 > 1.7e+34,
  * generated at construction time.
  * @type {string}
  */
-const clientId      = Math.random().toString(36).slice(2) + Math.random().toString(36).slice(2);
+const clientId    = stamp() + stamp();
