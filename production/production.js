1de8bd530df9df9fd1d1a8bfd8a8c1af8eb9f553
Dierk Koenig <dierk.koenig@canoo.com>
2022-01-03 00:35:47 +0100

    Housekeeping: update production

--- production/production.js
+++ production/production.js
@@ -3,6 +3,7 @@ import '../src/kolibri/stdlib.js';
 import '../src/kolibri/util/arrayFunctions.js';
 import '../src/kolibri/util/array.js';
 import '../src/kolibri/util/dom.js';
+import '../src/kolibri/rest/restClient.js';
 import '../src/kolibri/observable.js';
 import '../src/kolibri/presentationModel.js';
 import '../src/kolibri/version.js';
--- production/productionBundle.js
+++ production/productionBundle.js
@@ -496,6 +496,51 @@ const fireEvent = (element, eventTypeString) => {
  * @param { HTMLElement } element - The "target" element that fires the event.
  */
 const fireChangeEvent = element => fireEvent(element, CHANGE);/**
+ * Representing the client of an HTTP request.
+ * @param { !String } url - the url to fetch as a string. Mandatory.
+ * @param { "GET"|"PUT"|"POST"|"DELETE"|"HEAD"|"OPTION" } method - HTTP request method, default: "GET"
+ * @param { ?Object} data - payload data for PUT or POST requests, will be converted to JSON
+ * @return { Promise<JSON> } a promise that is either
+ *          rejected with an error code because the fetch failed or
+ *          resolved with the JSON payload being parsed.
+ * @example
+   client(URL)
+   .then( data => {
+       console.log(data.firstname); // work with data
+   })
+   .catch( err => console.error(err));
+ */
+const client = (url, method = 'GET', data = null) => {
+    const request = {
+        method:      method,             // *GET, POST, PUT, DELETE, etc.
+        mode:        'same-origin',      // no-cors, *cors, same-origin
+        cache:       'no-cache',         // no-cache, reload, force-cache, only-if-cached
+        credentials: 'same-origin',      // include, *same-origin, omit
+        headers: {
+            'Content-Type': 'application/json', // 'application/x-www-form-urlencoded'
+        },
+        redirect:    'follow',             // manual, *follow, error
+        referrer:    'no-referrer',        // no-referrer, *client
+    };
+    if (null != data) {
+        request.body = JSON.stringify(data);
+    }
+    return fetch(url, request)
+        .then(resp => {                             // fetch API cares for the general error handling
+            if (Number(resp.status) === 204) {
+                console.log("got special", 204);    // special: Grails returns this on successful DELETE
+                return Promise.resolve("ok");
+            }
+            if (resp.ok) {
+                return resp.json()
+            }
+            if (Number(resp.status) < 400) {
+                console.log("status", resp.status);
+                return resp.text();
+            }
+            return Promise.reject(resp.status);
+        })
+};/**
  * @template T - the generic value type for an {@link IObservable<T>}.
  * @typedef {*} T - the generic value type is unconstrained.
  */
@@ -860,11 +905,20 @@ const Attribute = (value, qualifier) => {
     };
 
     return { getObs, hasObs, setValidator, setConverter, setConvertedValue, getQualifier, setQualifier }
-};const release       = "0.1.33";
+};const release     = "0.1.35";
 
-const dateStamp     = "2021-12-29 T 18:26:49 MEZ";
+const dateStamp   = "2022-01-03 T 00:05:36 MEZ";
 
-const versionInfo   = release + " at " + dateStamp;/**
+const versionInfo = release + " at " + dateStamp;
+
+const stamp       = () => Math.random().toString(36).slice(2).padEnd(11,"X").slice(0,11);
+
+/**
+ * An constant random string of 22 lowercase characters/digits, probability: 1 of 36 ** 22 > 1.7e+34,
+ * generated at construction time.
+ * @type { String }
+ */
+const clientId    = stamp() + stamp();/**
  * @typedef { object } InputAttributes
  * @template T
  * @property { !T } value      - mandatory value, will become the input value, defaults to undefined
