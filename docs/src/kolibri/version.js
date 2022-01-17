fce2f0367c7a811225cd8c277ed515f131f0f9bb
Dierk Koenig <dierk.koenig@canoo.com>
2022-01-17 13:43:45 +0100

    improve jsdoc in version.js

--- docs/src/kolibri/version.js
+++ docs/src/kolibri/version.js
@@ -11,6 +11,8 @@ const stamp       = () => Math.random().toString(36).slice(2).padEnd(11,"X").sli
 /**
  * A constant random string of 22 lowercase characters/digits, probability: 1 of 36 ** 22 > 1.7e+34,
  * generated at construction time.
+ * The typical use case is to identify the client in a team application / multi-user environment
+ * such that value changes can be properly attributed and conflicts can be avoided.
  * @type { String }
  */
 const clientId    = stamp() + stamp();
