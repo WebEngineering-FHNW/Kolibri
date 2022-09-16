21972262f49d49653e3f2b0e7f6a32633d63674f
Fabian Affolter <fabian.affolter@fhnw.ch>
2022-09-16 15:09:13 +0200

    Added ObservableTypeString  - DESIGNSYSTEM  - EMPHASIS  - STATE

--- docs/src/kolibri/presentationModel.js
+++ docs/src/kolibri/presentationModel.js
@@ -8,13 +8,24 @@ import { id }         from "./stdlib.js";
 export { Attribute, QualifiedAttribute,
          presentationModelFromAttributeNames,
          valueOf, readQualifierValue,
+         VALID, VALUE, EDITABLE, LABEL, NAME, TYPE , DESIGNSYSTEM, EMPHASIS, STATE }
          VALID, VALUE, EDITABLE, LABEL, NAME, TOOLTIP, TYPE }
 
 /**
+ * @typedef {'value'|'valid'|'editable'|'label'|'name'|'type'|'designSystem'|'emphasis'|'state'} ObservableTypeString
  * @typedef {'value'|'valid'|'editable'|'label'|'name'|'tooltip'|'type'} ObservableTypeString
  * Feel free to extend this type with new unique type strings as needed for your application.
  */
 
+/** @type ObservableTypeString */ const VALUE           = "value";
+/** @type ObservableTypeString */ const VALID           = "valid";
+/** @type ObservableTypeString */ const EDITABLE        = "editable";
+/** @type ObservableTypeString */ const LABEL           = "label";
+/** @type ObservableTypeString */ const NAME            = "name";
+/** @type ObservableTypeString */ const TYPE            = "type"; // HTML input types: text, number, checkbox, etc. HTML Button types: submit, reset, button.
+/** @type ObservableTypeString */ const DESIGNSYSTEM    = "designSystem";
+/** @type ObservableTypeString */ const EMPHASIS        = "emphasis";
+/** @type ObservableTypeString */ const STATE           = "state";
 /** @type ObservableTypeString */ const VALUE    = "value";
 /** @type ObservableTypeString */ const VALID    = "valid";
 /** @type ObservableTypeString */ const EDITABLE = "editable";
