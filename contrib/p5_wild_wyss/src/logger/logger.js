90638fc78e032eca127a6c691a6a92c06d30d02d
Andri Wild <andri.wild@students.fhnw.ch>
2022-09-24 15:46:21 +0200

    remove state in logger function

--- contrib/p5_wild_wyss/src/logger/logger.js
+++ contrib/p5_wild_wyss/src/logger/logger.js
@@ -1,16 +1,14 @@
-export {Logger, enableLogger, disableLogger}
+export {Logger}
 import {LazyIf, Then, Else, True, False } from "../../../p6_brodwolf_andermatt/src/lambda-calculus-library/lambda-calculus.js";
 
-const Logger = callback => msg =>
-  LazyIf( loggerOn )
+const Logger = callback => msg => state =>
+  LazyIf( convertToChurchBool(state) )
     (Then( () => execute(callback)(msg)  ))
     (Else( () => False                   ));
+
+const convertToChurchBool = state => state ? True : False;
 
 const execute = callback => msg => {
   callback(msg);
   return True;
 };
-
-let loggerOn = False;
-const disableLogger = () => loggerOn = False;
-const enableLogger  = () => loggerOn = True;
