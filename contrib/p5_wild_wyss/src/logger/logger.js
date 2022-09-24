79a65b358b7d6d0ef60ec4bd9a8ff7a711ac0d40
Andri Wild <andri.wild@students.fhnw.ch>
2022-09-24 16:13:08 +0200

    add comments to the logger function

--- contrib/p5_wild_wyss/src/logger/logger.js
+++ contrib/p5_wild_wyss/src/logger/logger.js
@@ -1,6 +1,17 @@
 export {Logger}
 import {LazyIf, Then, Else, True, False } from "../../../p6_brodwolf_andermatt/src/lambda-calculus-library/lambda-calculus.js";
 
+/**
+ * The Logger function yields a custom configured log function.
+ *
+ * @param {function} callback caused by a logger call
+ * @param {String} msg the processed msg
+ * @param {boolean} state the logger's on/off state
+ * @return { function(x:String): function(y:boolean): {f: { y x }} }
+ * @example
+ * const log = Logger(msg => console.log(msg));
+ * log("action")(true);
+ */
 const Logger = callback => msg => state =>
   LazyIf( convertToChurchBool(state) )
     (Then( () => execute(callback)(msg)  ))
