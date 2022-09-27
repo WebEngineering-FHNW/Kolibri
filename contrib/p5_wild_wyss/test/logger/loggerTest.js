2467871e68f98e54702a5823571b15b3174b3431
Tobias Wyss <towyss@gmail.com>
2022-09-27 09:36:26 +0200

    add tests for loglevels

--- contrib/p5_wild_wyss/test/logger/loggerTest.js
+++ contrib/p5_wild_wyss/test/logger/loggerTest.js
@@ -1,5 +1,5 @@
 import {TestSuite} from "../../../../docs/src/kolibri/util/test.js";
-import {Logger} from "../../src/logger/logger.js";
+import {DebugLogger, LOG_DEBUG, LOG_FATAL, LOG_NOTHING, LOG_TRACE, LOG_WARN} from "../../src/logger/logger.js";
 
 const loggerSuite = TestSuite("Logger");
 
@@ -7,46 +7,69 @@ loggerSuite.add("test simple logging", assert => {
   let realMsg = '';
   const write = msg => realMsg = msg;
   const logMessage = 'hello world';
-  const log = Logger(() => true)(write);
-  log(logMessage);
+  const debug = DebugLogger(() => LOG_DEBUG)(write);
+  debug(logMessage);
   assert.is(realMsg, 'hello world');
 });
 
 loggerSuite.add("test enabling logging", assert => {
-  let loggerEnabled = false;
+  let logLevel = LOG_NOTHING;
   let realMsg = '';
   const logMessage = 'hello world';
 
   const write = msg => realMsg = msg;
-  const log = Logger(() => loggerEnabled)(write);
+  const debug = DebugLogger(() => logLevel)(write);
 
   // logging should be disabled
-  log(logMessage);
+  debug(logMessage);
   assert.is(realMsg, '');
 
   // logging should be enabled
-  loggerEnabled = true;
-  log(logMessage);
+  logLevel = LOG_DEBUG;
+  debug(logMessage);
   assert.is(realMsg, logMessage);
 });
 
 loggerSuite.add("test disabling logging", assert => {
-  let loggerEnabled = true;
+  let logLevel = LOG_DEBUG;
   let realMsg = '';
   const logMessage = 'hello world';
 
   const write = msg => realMsg = msg;
-  const log = Logger(() => loggerEnabled)(write);
+  const debug = DebugLogger(() => logLevel)(write);
 
   // logging should be enabled
-  log(logMessage);
+  debug(logMessage);
   assert.is(realMsg, logMessage);
 
   // logging should be disabled
-  loggerEnabled = false;
+  logLevel = LOG_NOTHING;
   realMsg = '';
-  log(logMessage)(loggerEnabled);
+  debug(logMessage)(logLevel);
   assert.is(realMsg, '');
 });
+
+loggerSuite.add("log lower logging level, should log", assert => {
+  const logLevel = LOG_TRACE;
+  let realMsg = '';
+  const logMessage = 'hello world';
+  const debug = DebugLogger(() => logLevel)(msg => realMsg = msg);
+
+  // loglevel debug should also be logged, when LOG_TRACE is enabled
+  debug(logMessage);
+  assert.is(realMsg, logMessage);
+});
+
+loggerSuite.add("log higher logging level, should not log", assert => {
+  const logLevel = LOG_WARN;
+  let realMsg = '';
+  const logMessage = 'hello world';
+  const debug = DebugLogger(() => logLevel)(msg => realMsg = msg);
+
+  // loglevel debug should not log when LOG_FATAL is enabled
+  debug(logMessage);
+  assert.is(realMsg, '');
+});
+
 
 loggerSuite.run();
