54aa85fa9a1c713d8b4d04689cbc4e942844315d
Andri Wild <andri.wild@students.fhnw.ch>
2022-10-12 08:01:56 +0200

    update test cases of appenders

--- contrib/p5_wild_wyss/src/logger/appender/consoleAppenderTest.js
+++ contrib/p5_wild_wyss/src/logger/appender/consoleAppenderTest.js
@@ -2,14 +2,7 @@ import {TestSuite} from "../../../../../docs/src/kolibri/util/test.js";
 import {Appender} from "./consoleAppender.js";
 import {convertToJsBool} from "../lamdaCalculus.js";
 
-const {
-  trace,
-  debug,
-  info,
-  warn,
-  error,
-  fatal,
-} = Appender();
+const { trace, debug, info, warn, error, fatal } = Appender();
 
 const loggerSuite = TestSuite("Console Appender");
 
--- contrib/p5_wild_wyss/src/logger/appender/stringAppenderTest.js
+++ contrib/p5_wild_wyss/src/logger/appender/stringAppenderTest.js
@@ -2,7 +2,7 @@ import {TestSuite} from "../../../../../docs/src/kolibri/util/test.js";
 import {Appender} from "./stringAppender.js";
 import {convertToJsBool} from "../lamdaCalculus.js";
 
-const { debug, getValue, reset } = Appender();
+const { trace, debug, info, warn, error, fatal, getValue, reset } = Appender();
 
 const loggerSuite = TestSuite("String Appender");
 
@@ -14,11 +14,11 @@ loggerSuite.add("test add debug value to string appender", assert => {
 });
 
 loggerSuite.add("test add two values to string appender", assert => {
-  const result1 = debug("first");
-  const result2 = debug("second");
+  const result1 = debug("A");
+  const result2 = debug("B");
   assert.is(convertToJsBool(result1), true );
   assert.is(convertToJsBool(result2), true );
-  assert.is(getValue("-"), "first-second");
+  assert.is(getValue(""), "AB");
   reset();
 });
 
@@ -28,6 +28,33 @@ loggerSuite.add("test reset string appender", assert => {
   assert.is(getValue(""), "first");
   reset();
   assert.isTrue("" === String(getValue("")));
+});
+
+loggerSuite.add("test concatenate strings with a custom delimiter", assert => {
+  const result1 = debug("first");
+  const result2 = debug("second");
+  assert.is(convertToJsBool(result1), true );
+  assert.is(convertToJsBool(result2), true );
+  assert.is(getValue(" <-> "), "first <-> second");
+  reset();
+  assert.isTrue("" === String(getValue("")));
+});
+
+loggerSuite.add("test add all kind of levels to string appender", assert => {
+  const traceLogResult  = trace("trace");
+  const debugLogResult  = debug("debug");
+  const infoLogResult   = info("info");
+  const warnLogResult   = warn("warn");
+  const errorLogResult  = error("error");
+  const fatalLogResult  = fatal("fatal");
+  assert.is(convertToJsBool(traceLogResult),  true);
+  assert.is(convertToJsBool(debugLogResult),  true);
+  assert.is(convertToJsBool(infoLogResult),   true);
+  assert.is(convertToJsBool(warnLogResult),   true);
+  assert.is(convertToJsBool(errorLogResult),  true);
+  assert.is(convertToJsBool(fatalLogResult),  true);
+  assert.is(getValue("-"),  "trace-debug-info-warn-error-fatal");
+  reset();
 });
 
 loggerSuite.run();
