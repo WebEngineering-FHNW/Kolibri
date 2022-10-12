4aeb448a4c82c0b11b1cbf437af572e63ab0d39b
Andri Wild <andri.wild@students.fhnw.ch>
2022-10-12 07:43:58 +0200

    update countAppenderTest with an all level test case

--- contrib/p5_wild_wyss/src/logger/appender/countAppenderTest.js
+++ contrib/p5_wild_wyss/src/logger/appender/countAppenderTest.js
@@ -2,7 +2,16 @@ import {TestSuite} from "../../../../../docs/src/kolibri/util/test.js";
 import {Appender} from "./countAppender.js";
 import {convertToJsBool} from "../lamdaCalculus.js";
 
-const { debug, getValue, reset } = Appender();
+const {
+  trace,
+  debug,
+  info,
+  warn,
+  error,
+  fatal,
+  getValue,
+  reset,
+} = Appender();
 
 const loggerSuite = TestSuite("Count Appender");
 
@@ -13,7 +22,7 @@ loggerSuite.add("test add debug value to count appender", assert => {
   reset();
 });
 
-loggerSuite.add("test add two values to string appender", assert => {
+loggerSuite.add("test add two values to count appender", assert => {
   const result1 = debug("first");
   const result2 = debug("second");
   assert.is(convertToJsBool(result1), true );
@@ -22,12 +31,40 @@ loggerSuite.add("test add two values to string appender", assert => {
   reset();
 });
 
-loggerSuite.add("test reset string appender", assert => {
+loggerSuite.add("test reset count appender", assert => {
   const result1 = debug("first");
   assert.is(convertToJsBool(result1), true );
   assert.is(getValue().debug, 1);
   reset();
   assert.isTrue(0 === getValue().debug );
+});
+
+loggerSuite.add("test add all kind of levels to count appender", assert => {
+  const traceResult  = trace("trace");
+  const debugResult  = debug("debug");
+  const infoResult   = info("info");
+  const warnResult   = warn("warn");
+  const errorResult  = error("error");
+  const fatalResult  = fatal("fatal");
+  assert.is(convertToJsBool(traceResult),  true);
+  assert.is(convertToJsBool(debugResult),  true);
+  assert.is(convertToJsBool(infoResult),   true);
+  assert.is(convertToJsBool(warnResult),   true);
+  assert.is(convertToJsBool(errorResult),  true);
+  assert.is(convertToJsBool(fatalResult),  true);
+  assert.is(getValue().trace,  1);
+  assert.is(getValue().debug,  1);
+  assert.is(getValue().info,   1);
+  assert.is(getValue().warn,   1);
+  assert.is(getValue().error,  1);
+  assert.is(getValue().fatal,  1);
+  reset();
+  assert.isTrue(0 === getValue().trace);
+  assert.isTrue(0 === getValue().debug);
+  assert.isTrue(0 === getValue().info);
+  assert.isTrue(0 === getValue().warn);
+  assert.isTrue(0 === getValue().error);
+  assert.isTrue(0 === getValue().fatal);
 });
 
 loggerSuite.run();
