21fcf9c71c26c4b53560c80ea1339d68c8cd3eb6
Andri Wild <andri.wild@students.fhnw.ch>
2022-10-26 13:07:31 +0200

    add jsDoc to LogUiProjectors

--- contrib/p5_wild_wyss/src/logger/loggingUi/logUiProjector.js
+++ contrib/p5_wild_wyss/src/logger/loggingUi/logUiProjector.js
@@ -8,28 +8,35 @@ export {
 import { fst, snd }   from "../../../../../docs/src/kolibri/stdlib.js";
 import { forEach }    from "../../../../p6_brodwolf_andermatt/src/stack/stack.js";
 
+/**
+ * Projects the filtered log messages to the ui.
+ *
+ * @param { HTMLElement } rootElement
+ * @param { stack }       stack
+ */
 const logMessagesProjector = (rootElement, stack) => {
-  rootElement.innerHTML = "";
+  rootElement.innerHTML   = "";
 
-  const createPreElement = (tuple, _) => {
-    const line      = document.createElement("PRE");
+  const createPreElement  = (tuple, _) => {
+    const line            = document.createElement("PRE");
+    line.innerHTML        = tuple(snd);
     line.classList.add("logMessage");
-    line.innerHTML  = tuple(snd);
     rootElement.appendChild(line);
     rootElement.scrollTo(0, rootElement.scrollHeight);
 
   };
-
   forEach(stack)(createPreElement);
 };
 
 /**
+ * Creates a label and an associated input element
+ * parameterized by passing parameters.
  *
- * @param { String }  type
- * @param { String }  labelText
- * @param { String }  id
- * @param { String }  placeholder
- * @return {HTMLCollection}
+ * @param   { String }  type
+ * @param   { String }  labelText
+ * @param   { String }  id
+ * @param   { String }  placeholder
+ * @return  { HTMLCollection }
  */
 const createLabeledInputElement = (type, labelText, id, placeholder) => {
     const labelClass = "text" === type ? "textLabel" : "";
@@ -41,6 +48,12 @@ const createLabeledInputElement = (type, labelText, id, placeholder) => {
     return template.children;
 };
 
+/**
+ * Projects a filter input field to the ui.
+ *
+ * @param   { LogUiController }  controller
+ * @return  { [Element,Element] } - label & input Element
+ */
 const textFilterProjector = controller => {
 
   const [label, input] = createLabeledInputElement(
@@ -54,6 +67,12 @@ const textFilterProjector = controller => {
   return [label, input];
 };
 
+/**
+ * Projects a global context input field to the ui.
+ *
+ * @param   { LogUiController }  controller
+ * @return  { [Element,Element] } - label & input Element
+ */
 const contextInputProjector = controller => {
 
   const [label, input] = createLabeledInputElement(
@@ -66,6 +85,13 @@ const contextInputProjector = controller => {
   return [label, input];
 };
 
+/**
+ * Projects toggle buttons for each log level to the ui.
+ *
+ * @param { HTMLElement }           rootElement
+ * @param { LogUiController }       controller
+ * @param { [LogLevelFilterType] }  levels
+ */
 const levelFilterProjector = (rootElement, controller, levels) => {
   rootElement.innerHTML = '';
   levels.forEach(checkBoxPair =>
@@ -74,10 +100,11 @@ const levelFilterProjector = (rootElement, controller, levels) => {
 };
 
 /**
+ * Creates a toggle button and an associated label.
  *
- * @param { } controller
- * @param checkBoxPair
- * @return {HTMLElement}
+ * @param   { LogUiController}      controller
+ * @param   { LogLevelFilterType }  checkBoxPair
+ * @return  { HTMLElement } - span which includes a label and a checkbox
  */
 const labeledCheckbox = (controller, checkBoxPair) => {
   const checkboxRoot  = document.createElement("SPAN");
@@ -93,13 +120,12 @@ const labeledCheckbox = (controller, checkBoxPair) => {
 
   checkbox.checked = checkBoxPair(snd);
   checkBoxPair(snd)
-      ? checkboxRoot.classList.remove("checkedSpan")
-      : checkboxRoot.classList.add("checkedSpan");
+      ? checkboxRoot.classList.remove ("checkedSpan")
+      : checkboxRoot.classList.add    ("checkedSpan");
 
   checkboxRoot.append(checkbox, label);
-  checkboxRoot.onclick = _ => {
+  checkboxRoot.onclick = _ =>
     controller.flipLogLevel(checkBoxPair);
-  };
 
   return checkboxRoot;
 };
