7bc471a255e150556489095d8f079ca8c8d89f69
Andri Wild <andri.wild@students.fhnw.ch>
2022-11-02 10:55:59 +0100

    add benchmark test application

---
+++ contrib/p5_wild_wyss/src/logger/logUi/benchmark/benchmark.js
@@ -0,0 +1,25 @@
+
+console.log("start");
+const results = [];
+
+const value = [];
+
+for (let i = 0; i < 1000; i++) {
+  for (let j = 0; j < 1000; j++) {
+    value.push("i");
+  }
+
+
+  const start = self.performance.now();
+  for (let k = 0; k < 1000; k++) {
+    value.push("i");
+    value.shift();
+  }
+  const end = self.performance.now();
+  self.postMessage(end-start);
+
+}
+
+
+
+console.log("end");
