32269f3e3595ad6717b289d5b68c8f22e88fd3fa
Andri Wild <andri.wild@students.fhnw.ch>
2022-11-11 08:05:02 +0100

    update sample log message

--- contrib/p5_wild_wyss/src/logger/logUi/example/example.js
+++ contrib/p5_wild_wyss/src/logger/logUi/example/example.js
@@ -25,7 +25,7 @@ setInterval(() => {
       logger1.trace, logger1.debug, logger1.info, logger1.warn, logger1.error, logger1.fatal,
       logger2.trace, logger2.info, logger2. debug, logger2.warn, logger2.error, logger2.fatal
   ];
-  const msg = ["hello world", "Tobias Wyss", "Andri Wild", "IP5", "wild animals",  "into the wild", "#biersaufen 🏍"];
+  const msg = ["hello world", "Tobias Wyss", "Andri Wild", "IP5", "wild animals",  "into the wild", "# drive 🏍 "];
   loggers[Math.floor(Math.random() * loggers.length)](msg[Math.floor(Math.random()* msg.length)]);
 
 }, 2000);
