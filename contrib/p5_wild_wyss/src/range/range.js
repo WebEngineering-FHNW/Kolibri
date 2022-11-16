480aa1c0dca2767dd813dc4830122dbe40680f8a
Andri Wild <andri.wild@students.fhnw.ch>
2022-11-16 14:17:49 +0100

    add range implementation

---
+++ contrib/p5_wild_wyss/src/range/range.js
@@ -0,0 +1,84 @@
+export {Range}
+
+/**
+ *
+ * @param  { Number } a
+ * @param  { Number } b
+ * @return { [Number, Number] }
+ */
+const sort = (a, b) => {
+  if (a < b) return [a,b];
+  else return [b,a];
+}
+
+/**
+ *
+ * @param   { Number } stepIsNegative
+ * @param   { Number } next
+ * @param   { Number } end
+ * @return  { boolean }
+ */
+const hasReachedEnd = (stepIsNegative, next, end) =>
+    stepIsNegative ? next < end : next > end;
+
+/**
+ *
+ * @param   { Number }  from
+ * @param   { Number }  to
+ * @param   { Boolean } stepIsNegative
+ * @return  { [Number, Number] }
+ */
+const initBoundaries = (from, to, stepIsNegative) => {
+  let [min, max] = sort(from, to);
+  let next = min;
+  let end  = max;
+  if (stepIsNegative) {
+    next = max;
+    end = min;
+  }
+  return [next, end];
+}
+
+/**
+ *
+ *
+ * @constructor
+ * @param { Number } from
+ * @param { Number } to
+ * @param { Number } step
+ * @returns {
+ *    {
+ *      next:   () =>  IteratorResult,
+ *      return: () =>  IteratorResult,
+ *      [Symbol.iterator](): this
+ *    }
+ *  }
+ *  @example
+ *  const [zero, one, two, three] = Range(3);
+ *  const [five, three, one]      = Range(1, 5, -2);
+ *  const [three, four, five]     = Range(5, 3);
+ */
+const Range = (from, to = 0, step = 1) => {
+  const stepIsNegative  = step < 0;
+  let   done            = false;
+  let   [next, end]     = initBoundaries(from, to, stepIsNegative);
+
+  return {
+    next: () => {
+      if (done) return { done, value: next };
+
+      const tmp = next;
+      next += step;
+
+      done = hasReachedEnd(stepIsNegative, next, end)
+      return { done: false, value: tmp };
+    },
+    return: () => {
+      done = true;
+      return { done, value: next };
+    },
+    [Symbol.iterator]() {
+      return this;
+    },
+  };
+}
---
+++ contrib/p5_wild_wyss/src/range/rangeTest.js
@@ -0,0 +1,81 @@
+import { Range } from "./range.js"
+import {TestSuite} from "../../../../docs/src/kolibri/util/test.js";
+
+const loggerSuite = TestSuite("Range");
+
+
+const testRange = (from, to, step, range, assert) => {
+
+  for (let expected = from; expected <= to; expected += step) {
+    const { done, value } = range.next();
+    assert.is(value, expected)
+    assert.isTrue(!done)
+  }
+  assert.isTrue(range.next().done)
+}
+
+const testRangeNegativeStepSize = (from, to, step, range, assert) => {
+  for (let expected = from; expected >= to; expected += step) {
+    const { done, value } = range.next();
+    assert.is(value, expected)
+    assert.isTrue(!done)
+  }
+  assert.isTrue(range.next().done)
+}
+
+loggerSuite.add("test simple Range(3)", assert => {
+  testRange(0, 3, 1, Range(3), assert)
+});
+
+loggerSuite.add("test simple Range(2,3)", assert => {
+  testRange(2, 3, 1, Range(2,3), assert)
+});
+
+loggerSuite.add("test simple Range(3,2,1)", assert => {
+  testRange(2, 3, 1, Range(3,2,1), assert)
+});
+
+loggerSuite.add("test break Range(7)", assert => {
+  const range = Range(7);
+
+  for (const value of range) {
+    assert.is(value, 0);
+    break;
+  }
+  assert.isTrue(range.next().done)
+});
+
+loggerSuite.add("test use range twice", assert => {
+  const range = Range(0);
+
+  const [zero] = range;
+  assert.is(zero, 0);
+
+  const [repeat] = range;
+  assert.is(repeat, undefined)
+});
+
+loggerSuite.add("test use range twice", assert => {
+  for (const value of Range(10)) {
+    if(value < 4) continue;
+    if(value > 4) break;
+    assert.is(value, 4);
+  }
+});
+
+loggerSuite.add("test negative Range(4,6,-2)", assert => {
+  testRangeNegativeStepSize(6, 4, -2, Range(4, 6, -2), assert)
+});
+
+loggerSuite.add("test negative Range(6,4,-2)", assert => {
+  testRangeNegativeStepSize(6, 2, -2, Range(6, 2, -2), assert)
+});
+
+loggerSuite.add("test negative Range(0, -2, -1)", assert => {
+  testRangeNegativeStepSize(0, -2, -1, Range(0, -2, -1), assert)
+});
+
+loggerSuite.run();
+
+// negRange.forEach(console.log); // forEach does not exist on JS Iterator until now
+// negRange.forEach(console.log); // never shown
