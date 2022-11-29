6999fefaf429ceb2eff35ee219ab6238bc072662
Dierk Koenig <dierk.koenig@canoo.com>
2022-11-29 14:49:05 +0100

    chore: jsDoc for iterator.js

--- contrib/p5_wild_wyss/src/iterator/iterator.js
+++ contrib/p5_wild_wyss/src/iterator/iterator.js
@@ -1,7 +1,26 @@
 
 export { Iterator }
 
-// startValue: T, incrementFunction: T => T, stopDetected: T => Boolean
+
+
+/** @typedef  IteratorType
+ * @template _T_
+ * @property { () => { next: () => IteratorResult } }            [Symbol.iterator]
+ * @property { (callback:Consumer<_T_>)   => void }              forEach   - executes the callback for each element and exhausts the iterator
+ * @property { (predicate:Predicate<_T_>) => IteratorType<_T_> } dropWhile - proceed with the iteration where the predicate no longer holds
+ * @property { (count:Number)             => IteratorType<_T_> } drop      - jump over so many elements
+ * @property { (predicate:Predicate<_T_>) => IteratorType<_T_> } takeWhile - proceed with the iteration until the predicate becomes true
+ * @property { (count:Number)             => IteratorType<_T_> } take      - stop after so many elements
+ */
+
+/**
+ * @template _T_
+ * @param { _T_ }               value
+ * @param { (_T_) => _T_ }      incrementFunction
+ * @param { (_T_) => Boolean }  stopDetected - returns true if the iteration should stop
+ * @return { IteratorType<_T_> }
+ * @constructor
+ */
 const Iterator = (value, incrementFunction, stopDetected) => {
 
   const next = () => {
