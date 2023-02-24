9179ac78e1b96b213e9e3b70f3d4e9d8b28035bc
Andri Wild <andri.wild@students.fhnw.ch>
2023-02-24 15:28:41 +0100

    feat(iterator): add TupleIterator

--- contrib/wild_wyss/src/iteratorFunctions/iterator.js
+++ contrib/wild_wyss/src/iteratorFunctions/iterator.js
@@ -1,4 +1,13 @@
-export { nextOf, createIterator, Iterator, ArrayIterator, emptyIterator }
+export { nextOf, createIterator, Iterator, ArrayIterator, TupleIterator, emptyIterator }
+
+import { map } from "./iteratorFunctions.js"
+const pipe = copy => (...transformers) => {
+  let it = copy();
+  for (const transformer of transformers) {
+    it = transformer(it);
+  }
+  return it;
+};
 
 const Iterator = (value, inc, stop) => {
 
@@ -21,6 +30,15 @@ const Iterator = (value, inc, stop) => {
 const ArrayIterator = array =>
   map(i => array[i])(Iterator(0, x => x + 1, x => x === array.length));
 
+const TupleIterator = tuple => {
+  // detect number of elements in tuple using a special selector function
+  const lengthSelector = arr => arr.length;
+  // TODO: replace Iterator with Range
+  const indexIterator  = Iterator(0, i => i + 1, i => i >= tuple(lengthSelector));
+  // map over indices and grab corresponding element from tuple
+  return map(idx => tuple(values => values[idx]))(indexIterator);
+};
+
 const emptyIterator =
   Iterator(undefined, _ => undefined, _ => true);
 
@@ -35,12 +53,5 @@ const createIterator = next => iteratorFunction => (...params) => innerIterator 
   };
 };
 
-const pipe = copy => (...transformers) => {
-  let it = copy();
-  for (const transformer of transformers) {
-    it = transformer(it);
-  }
-  return it;
-};
 
 const nextOf = it => it[Symbol.iterator]().next();
--- contrib/wild_wyss/src/iteratorFunctions/iteratorFunctionsTest.js
+++ contrib/wild_wyss/src/iteratorFunctions/iteratorFunctionsTest.js
@@ -1,18 +1,19 @@
 import { TestSuite }                from "../../../../docs/src/kolibri/util/test.js";
 import { arrayEq }                  from "../../../../docs/src/kolibri/util/arrayFunctions.js";
 import { map, filter }              from "./iteratorFunctions.js";
-import { Iterator, ArrayIterator }  from "./iterator.js"
+import { Iterator, ArrayIterator, TupleIterator }  from "./iterator.js"
+import { Tuple }                    from "../../../../docs/src/kolibri/stdlib.js";
 
 const newIterator = limit => Iterator(0, current => current + 1, current => current > limit);
 
 const iteratorSuite = TestSuite("IteratorFunctions");
-iteratorSuite.add("test simple map", assert => {
+iteratorSuite.add("test typical case: map", assert => {
   const it = newIterator(4);
   const mapped = map(el => el * 2)(it);
   assert.is(arrayEq([0, 2, 4, 6, 8])([...mapped]), true)
 });
 
-iteratorSuite.add("test complex map", assert => {
+iteratorSuite.add("test advanced case: map", assert => {
   const it = newIterator(4);
   const mapped = map(el => el * 2)(it);
   const copyMapped = mapped.copy();
@@ -21,13 +22,13 @@ iteratorSuite.add("test complex map", assert => {
   assert.is(arrayEq([0, 2, 4, 6, 8])([...copyMapped]), true);
   assert.is(arrayEq([0, 4, 8, 12, 16])([...copyMappedMapped]), true);
 });
-iteratorSuite.add("test simple retainAll", assert => {
+iteratorSuite.add("test typical case: retainAll", assert => {
   const iterator = newIterator(4);
   const filtered = filter(el => el % 2 === 0)(iterator);
   assert.is(arrayEq([0, 2, 4])([...filtered]), true)
 });
 
-iteratorSuite.add("test simple retainAll", assert => {
+iteratorSuite.add("test typical case: retainAll", assert => {
   const iterator = newIterator(4);
   const filtered = filter(el => el % 2 === 0)(iterator);
   const copyFiltered = filtered.copy();
@@ -38,7 +39,7 @@ iteratorSuite.add("test simple retainAll", assert => {
   assert.is(arrayEq([4])([...mappedFiltered]), true);
 });
 
-iteratorSuite.add("test simple retainAll", assert => {
+iteratorSuite.add("test typical case: retainAll", assert => {
   const iterator = newIterator(4);
   const piped = iterator.pipe(
     map(i => i + 1),
@@ -49,19 +50,37 @@ iteratorSuite.add("test simple retainAll", assert => {
 });
 
 
-iteratorSuite.add("test simple array iterator", assert => {
+iteratorSuite.add("test typical case: array iterator", assert => {
   const  arrayIterator = ArrayIterator([1,2,3]);
 
   assert.is(arrayEq([1,2,3])([...arrayIterator]), true);
 });
 
-iteratorSuite.add("test complex array iterator", assert => {
+iteratorSuite.add("test advanced case: array iterator", assert => {
   const arrayIterator = ArrayIterator([1,2,3]);
   const pipedArrayIterator = arrayIterator.pipe(
     map(i => i + 1),
     filter(el => el % 2 === 0)
   );
   assert.is(arrayEq([2,4])([...pipedArrayIterator]), true);
+});
+
+iteratorSuite.add("test typical case: tuple iterator", assert => {
+  const [ Triple ] = Tuple(3);
+  const triple = Triple(1)(2)(3);
+  const tupleIterator = TupleIterator(triple);
+  assert.is(arrayEq([1,2,3])([...tupleIterator]), true);
+});
+
+iteratorSuite.add("test advanced case: tuple iterator", assert => {
+  const [ Triple ] = Tuple(3);
+  const triple = Triple(1)(2)(3);
+  const tupleIterator = TupleIterator(triple);
+  const pipedTupleIterator = tupleIterator.pipe(
+    map(i => i + 1),
+    filter(el => el % 2 === 0)
+  );
+  assert.is(arrayEq([2,4])([...pipedTupleIterator]), true);
 });
 
 iteratorSuite.run();
