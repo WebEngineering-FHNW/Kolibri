31da4cae2d33e3856cc5e47b6f775dc471b88e32
Andri Wild <andri.wild@students.fhnw.ch>
2023-02-25 18:31:00 +0100

    feat(iterator): add iterator operations: eq$ head isEmpty drop dropWhile reverse$ concat$ cons$

--- contrib/wild_wyss/src/iteratorFunctions/iteratorFunctions.js
+++ contrib/wild_wyss/src/iteratorFunctions/iteratorFunctions.js
@@ -1,6 +1,18 @@
-import { createIterator, nextOf } from "./iterator.js";
+import { ArrayIterator, createIterator, nextOf } from "./iterator.js";
+import { arrayEq }                               from "../../../../docs/src/kolibri/util/arrayFunctions.js";
 
-export { map, filter }
+export {
+  map,
+  filter,
+  eq$,
+  head,
+  isEmpty,
+  dropWhile,
+  drop,
+  reverse$,
+  concat$,
+  cons$,
+}
 
 /**
  * @template _T_
@@ -55,4 +67,144 @@ const filter = predicate => iterator => {
   };
 
   return createIterator(next)(filter)(predicate)(inner);
+};
+
+/**
+ * @function
+ * @pure
+ * @type {
+ *             (it1: IteratorType)
+ *          => (it2: IteratorType)
+ *          => boolean
+ *       }
+ * @example
+ * const it1    = Iterator(0, inc, stop);
+ * const it2    = Iterator(0, inc, stop);
+ * const result = eq$(it1)(it2);
+ */
+const eq$ = it1 => it2 =>
+  arrayEq([...it1.copy()])([...it2.copy()]);
+
+/**
+ * @function
+ * @pure
+ * @type { (it1: IteratorType) => any }
+ * @example
+ * const it     = Iterator(0, inc, stop);
+ * const result = head(it);
+ */
+const head = iterator => {
+  const inner = iterator.copy();
+  const { done, value } = nextOf(inner);
+  return done ? undefined : value;
+};
+
+/**
+ * @function
+ * @pure
+ * @param   { IteratorType } iterator
+ * @returns { boolean }
+ * @example
+ * const it     = Iterator(0, inc, stop);
+ * const result = isEmpty(it);
+ */
+const isEmpty = iterator => head(iterator) === undefined;
+
+/**
+* @template _T_
+* @function
+* @pure
+* @type {
+*             (predicate: Predicate)
+  *          => (iterator: IteratorType)
+  *          => IteratorType<_T_>
+  *      }
+* @example
+* const it      = Iterator(0, inc, stop);
+* const dropped = dropWhile(el => el < 2)(it);
+*/
+const dropWhile = predicate => iterator => {
+  const inner = iterator.copy();
+
+  const next = () => {
+    let { done, value } = nextOf(inner);
+
+    while(predicate(value) && !done) {
+      const n = nextOf(inner);
+      done    = n.done;
+      value   = n.value;
+    }
+    return {
+      done,
+      value
+    }
+  };
+  return createIterator(next)(dropWhile)(predicate)(inner);
+};
+
+/**
+ * @template _T_
+ * @function
+ * @pure
+ * @type {
+ *             (count: number)
+ *          => (iterator: IteratorType)
+ *          => IteratorType<_T_>
+ *       }
+ * @example
+ * const it      = Iterator(0, inc, stop);
+ * const dropped = drop(2)(it);
+ */
+const drop = count => iterator => {
+  let i = 0;
+  return dropWhile(_ => i++ < count)(iterator);
+};
+
+/**
+ * @template _T_
+ * @function
+ * @pure
+ * @type { (iterator: IteratorType) => IteratorType<_T_> }
+ * @example
+ * const it       = Iterator(0, inc, stop);
+ * const reversed = reverse(it);
+ */
+const reverse$ = iterator => {
+  const values = [...iterator.copy()].reverse();
+  return ArrayIterator(values);
+};
+
+/**
+ * @template _T_
+ * @function
+ * @pure
+ * @type {
+ *             (it1: IteratorType)
+ *          => (it2: IteratorType)
+ *          => IteratorType<_T_>
+ *       }
+ * @example
+ * const it1     = Iterator(0, inc, stop);
+ * const it2     = Iterator(0, inc, stop);
+ * const concat = concat$(it1)(it2);
+ */
+const concat$ = it1 => it2 => ArrayIterator([...it1, ...it2]);
+
+/**
+ * @template _T_
+ * @function
+ * @pure
+ * @type {
+ *             (element: any)
+ *          => (it: IteratorType)
+ *          => IteratorType<_T_>
+ *       }
+ * @example
+ * const it      = Iterator(0, inc, stop);
+ * const element = 1;
+ * const cons    = cons$(element)(it2);
+ */
+const cons$ = a => iterator => {
+  const inner = iterator.copy();
+  return ArrayIterator([a, ...inner]);
 };
--- contrib/wild_wyss/src/iteratorFunctions/iteratorFunctionsTest.js
+++ contrib/wild_wyss/src/iteratorFunctions/iteratorFunctionsTest.js
@@ -1,8 +1,25 @@
-import { TestSuite }                from "../../../../docs/src/kolibri/util/test.js";
-import { arrayEq }                  from "../../../../docs/src/kolibri/util/arrayFunctions.js";
-import { map, filter }              from "./iteratorFunctions.js";
-import { Tuple }                    from "../../../../docs/src/kolibri/stdlib.js";
-import { Iterator, ArrayIterator, TupleIterator }  from "./iterator.js"
+import { TestSuite } from "../../../../docs/src/kolibri/util/test.js";
+import { arrayEq }   from "../../../../docs/src/kolibri/util/arrayFunctions.js";
+import { Tuple }     from "../../../../docs/src/kolibri/stdlib.js";
+
+import {
+  Iterator,
+  ArrayIterator,
+  TupleIterator
+}  from "./iterator.js"
+
+import {
+  map,
+  filter,
+  eq$,
+  head,
+  isEmpty,
+  dropWhile,
+  drop,
+  reverse$,
+  concat$,
+  cons$,
+} from "./iteratorFunctions.js";
 
 const newIterator = limit => Iterator(0, current => current + 1, current => current > limit);
 
@@ -50,7 +67,6 @@ iteratorSuite.add("test typical case: pipe", assert => {
   assert.is(arrayEq([2,4])([...piped]), true);
 });
 
-
 iteratorSuite.add("test typical case: ArrayIterator", assert => {
   const  arrayIterator = ArrayIterator([1,2,3]);
   assert.is(arrayEq([1,2,3])([...arrayIterator]), true);
@@ -81,6 +97,93 @@ iteratorSuite.add("test advanced case: tuple iterator", assert => {
     filter(el => el % 2 === 0)
   );
   assert.is(arrayEq([2,4])([...pipedTupleIterator]), true);
+});
+
+iteratorSuite.add("test typical case: eq$ should return true", assert => {
+  const it1 = newIterator(4);
+  const it2 = newIterator(4);
+  assert.is(eq$(it1)(it2), true);
+});
+
+iteratorSuite.add("test typical case: eq$ should return false", assert => {
+  const it1 = newIterator(2);
+  const it2 = newIterator(4);
+  assert.is(eq$(it1)(it2), false);
+});
+
+iteratorSuite.add("test advanced case: eq$ should return true", assert => {
+  const it1 = newIterator(4);
+  const it2 = newIterator(4);
+  for (const _ of it1) { /** Range gets exhausted. */ }
+  assert.is(eq$(it1)(it2), false);
+});
+
+iteratorSuite.add("test typical case: head", assert => {
+  const iterator = newIterator(4);
+  let iteratorHead = head(iterator);
+  for (const iteratorElement of iterator) {
+    assert.is(iteratorHead, iteratorElement);
+    iteratorHead = head(iterator);
+  }
+  assert.is(head(iterator), undefined);
+});
+
+iteratorSuite.add("test typical case: isEmpty", assert => {
+  const iterator = newIterator(4);
+  let result = isEmpty(iterator);
+  assert.is(result, false);
+  for (const _ of iterator) { /** Range gets exhausted. */ }
+  result = isEmpty(iterator);
+  assert.is(result, true);
+});
+
+iteratorSuite.add("test typical case: dropWhile", assert => {
+  const iterator = newIterator(4);
+  const dropped  = dropWhile(el => el < 2)(iterator);
+  assert.is(arrayEq([2, 3, 4])([...dropped]), true);
+});
+
+iteratorSuite.add("test advanced case: dropWhile", assert => {
+  const iterator = newIterator(4);
+  const result   = iterator.pipe(
+    dropWhile(el => el < 2),
+    map(el => el + 1)
+  );
+  assert.is(arrayEq([3, 4, 5])([...result]), true);
+});
+
+iteratorSuite.add("test typical case: drop", assert => {
+  const iterator = newIterator(4);
+  const dropped  = drop(2)(iterator);
+  assert.is(arrayEq([2, 3, 4])([...dropped]), true);
+});
+
+iteratorSuite.add("test advanced case: drop", assert => {
+  const iterator = newIterator(4);
+  const result   = iterator.pipe(
+    drop(2),
+    map(el => el + 1)
+  );
+  assert.is(arrayEq([3, 4, 5])([...result]), true);
+});
+
+iteratorSuite.add("test typical case: reverse$", assert => {
+  const iterator = newIterator(4);
+  const reversed = reverse$(iterator);
+  assert.is(arrayEq([4, 3, 2, 1, 0])([...reversed]), true);
+});
+
+iteratorSuite.add("test typical case: concat$", assert => {
+  const it1    = newIterator(1);
+  const it2    = newIterator(2);
+  const concat = concat$(it1)(it2);
+  assert.is(arrayEq([0, 1, 0, 1, 2])([...concat]), true);
+});
+
+iteratorSuite.add("test typical case: cons$", assert => {
+  const it   = newIterator(1);
+  const cons = cons$(7)(it);
+  assert.is(arrayEq([7, 0, 1])([...cons]), true);
 });
 
 iteratorSuite.run();
