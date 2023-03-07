ca8d82d47a6caf258401393752efbbb6b1b11551
Andri Wild <andri.wild@students.fhnw.ch>
2023-03-07 14:42:09 +0100

    test(iterator): add basic usage test

--- contrib/wild_wyss/src/iterator/iteratorTest.js
+++ contrib/wild_wyss/src/iterator/iteratorTest.js
@@ -18,6 +18,15 @@ const newIterator = limit => Iterator(0, current => current + 1, current => curr
 
 const iteratorSuite = TestSuite("Iterator");
 
+iteratorSuite.add("test typical case: Iterator", assert => {
+  const iterator = Iterator(0, current => current + 1, current => current > 5);
+  let currentElement = 0;
+  for (const iteratorElement of iterator) {
+    assert.is(currentElement++, iteratorElement);
+  }
+  assert.is(arrayEq([])([...iterator]), true);
+});
+
 iteratorSuite.add("test special case: no increment after done", assert => {
   let result = true;
   const iterator = Iterator(true, _ => result = false, _ => true);
