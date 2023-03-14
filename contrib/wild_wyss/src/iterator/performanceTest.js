97fc7fb56d9063cb9118cef82357acaabab7226b
Andri Wild <andri.wild@students.fhnw.ch>
2023-03-14 15:52:06 +0100

    test(iterator): add reduce big range test

--- contrib/wild_wyss/src/iterator/performanceTest.js
+++ contrib/wild_wyss/src/iterator/performanceTest.js
@@ -1,15 +1,22 @@
 import {TestSuite} from "../../../../docs/src/kolibri/util/test.js";
 import {emptyIterator} from "./iterator.js";
-import {cons, take} from "./intermediateOperations.js";
+import {cons, take, map} from "./intermediateOperations.js";
+import {reduce$} from "./terminalOperations.js";
+import {Range} from "../range/range.js";
 
 const iteratorSuite = TestSuite("PerformanceTest");
-iteratorSuite.add("", assert => {
+iteratorSuite.add("test many cons", assert => {
   let it = emptyIterator;
-  const limit = 1000000;
+  const limit = 12000;
   for (let i = 0; i < limit; i++) {
     it = cons(i)(it);
   }
-  console.log(...take(1)(it));
+  console.log(reduce$( (acc, cur) => acc + cur, 0)(it));
 });
 
+iteratorSuite.add("test reduce big range", assert => {
+ let it = Range(120_000_000);
+ it = map(el => el * 2)(it);
+  console.log("with range: " + reduce$( (acc, cur) => acc + cur, 0)(it));
+});
 iteratorSuite.run();
