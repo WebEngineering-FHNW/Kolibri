e5bf481ec2d7ce451de9de4628611083d22d2a9b
Dierk Koenig <dierk.koenig@canoo.com>
2023-03-17 17:57:04 +0100

    improve: apply the style sheet to the head and not to the container since the latter is not valid.

--- docs/src/kolibri/logger/logUi/createLogUi.js
+++ docs/src/kolibri/logger/logUi/createLogUi.js
@@ -1,16 +1,16 @@
 import {projectDebounceInput}                     from "../../projector/simpleForm/simpleInputProjector.js";
+import {SimpleInputController}                    from "../../projector/simpleForm/simpleInputController.js";
 import {dom}                                      from "../../util/dom.js";
 import {shadowCss}                                from "../../style/kolibriStyle.js";
-import {LogUiController}                          from "./logUiController.js";
 import {setLoggingContext}                        from "../logger.js";
-import {SimpleInputController}                    from "../../projector/simpleForm/simpleInputController.js";
+import {LogUiController}                          from "./logUiController.js";
 import {loggingSelectProjector}                   from "./logUiProjector.js";
 import {LogMessagesView, projectLogLevelControls} from "./logView.js";
 
 export { createLogUi }
 
 /**
- * Creates the log ui on a given html element.
+ * Creates the log ui under a given html element.
  *
  * @param { HTMLElement } rootElement
  * @param { String }      cssStyleUrl - url to the css file that is used for styling the log ui
@@ -51,14 +51,17 @@ const createLogUi = (rootElement, cssStyleUrl) => {
 
   configSection.append(...projectDebounceInput  ("context", simpleController, 200));
   configSection.append(...loggingSelectProjector(controller));
-  filterSection.append(...projectDebounceInput  ("filter", filterController, 200));
+  filterSection.append(...projectDebounceInput  ("filter",  filterController, 200));
   filterSection.append(loggerLevelFilterRoot);
 
-  const styleRoot = document.createElement("STYLE");
-  styleRoot.innerHTML = ` @import "${cssStyleUrl}"; /* inserted by createLogUi.js */ `;
+  const [styleElement] = dom(`
+        <link rel="stylesheet" type="text/css"  href="${cssStyleUrl}"
+              data-note="Dynamically inserted by createLogUi.js." 
+        />
+  `);
+  document.head.append(styleElement);
 
   rootElement.append(
-      styleRoot,
       configSection,
       filterSection,
       loggerMessageContainerRoot
