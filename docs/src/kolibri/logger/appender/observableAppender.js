6de286c0df941d921d03c71ba847675a0c6babe3
Dierk Koenig <dierk.koenig@canoo.com>
2023-03-18 17:09:26 +0100

    improve: reengineer the observableAppender.js and make the tests run. (The new version needs less tests)

--- docs/src/kolibri/logger/appender/observableAppender.js
+++ docs/src/kolibri/logger/appender/observableAppender.js
@@ -1,173 +1,36 @@
+/**
+ * @module logger/observableAppender
+ * The observable Appender is a decorator for other {@link AppenderType}s that notifies a listener about new log messages or
+ * other interesting events.
+ */
+import {LOG_DEBUG, LOG_ERROR, LOG_FATAL, LOG_INFO, LOG_NOTHING, LOG_TRACE, LOG_WARN} from "../logger.js";
+
 export { Appender}
 
-import { Observable }                       from "../../observable.js";
-
-// todo dk: do not rely on contrib
-import { emptyStack, push, size }           from "../../../../../contrib/p6_brodwolf_andermatt/src/stack/stack.js";
-import {Pair, id, T, LazyIf, churchBool}    from "../../lambda/church.js";
-import { jsNum }                            from "../../lambda/churchNumbers.js";
-import { LOG_DEBUG, LOG_ERROR, LOG_FATAL, LOG_INFO, LOG_TRACE, LOG_WARN,} from "../logger.js";
-
-const MAX_STACK_ELEMENTS    = Number.MAX_SAFE_INTEGER -1;
-const MIN_STACK_SIZE        = 2;
-
-const OVERFLOW_LOG_MESSAGE  =
-  "LOG ERROR: Despite running the chosen eviction strategy, the array was full! The first third of the log messages have been deleted!";
+/**
+ * @typedef { (logLevel: LogLevelType, msg: ?String) => void } AppendListenerType
+ * A log level of value {@link LOG_NOTHING} indicates that nothing has been logged, e.g. in a "reset".
+ */
 
 /**
- * This is the default function that gets called when the defined limit has been reached.
- * It will empty the whole stack.
- * @param   { IObservable } currentValue
- * @returns { IObservable }
+ * The observable Appender is a decorator for other {@link AppenderType}s
+ * that notifies a listener about log events that have been delegated to the decorated appender.
+ * @constructor
+ * @type {  <_T_>
+ *     (appender: AppenderType<_T_>) =>
+ *     (listener: AppendListenerType) =>
+ *     AppenderType<_T_>
+ * }
  */
-const DEFAULT_CACHE_EVICTION_STRATEGY =  currentValue => {
-  // clear stack
-  currentValue.setValue(emptyStack);
-  return currentValue;
-};
-
-/**
- * Provides an observable appender.
- * Use {@link getValue} to get the observable and register yourself on changes
- * and use {@link reset} to clear the array.
- * @param { Number                                } limit           - the max amount of log messages to keep.
- * @param { UnaryOperatorType<IObservable>        } cacheEvictionStrategy  - This function is called, as soon as the
- *      defined limit of log messages is reached. You obtain the current appender
- *      value. Return a new value which will be used as the new value of this appender.
- *      If this parameter is not set, then all log messages until now will be discarded.
- * @returns { AppenderType.<IObservable> }
- */
-const Appender = (limit = MAX_STACK_ELEMENTS, cacheEvictionStrategy = DEFAULT_CACHE_EVICTION_STRATEGY) => {
-  // make sure, the stack is not defined too small.
-  const calculatedLimit = MIN_STACK_SIZE < limit ? limit: MIN_STACK_SIZE;
-  return {
-    trace:  trace(calculatedLimit)(cacheEvictionStrategy),
-    debug:  debug(calculatedLimit)(cacheEvictionStrategy),
-    info:   info(calculatedLimit) (cacheEvictionStrategy),
-    warn:   warn(calculatedLimit) (cacheEvictionStrategy),
-    error:  error(calculatedLimit)(cacheEvictionStrategy),
-    fatal:  fatal(calculatedLimit)(cacheEvictionStrategy),
-    getValue,
-    reset
+const Appender = appender => listener => {
+  return /** @type {AppenderType} */ {
+    trace:  arg => { const x = appender.trace(arg); listener(LOG_TRACE, arg); return x },
+    debug:  arg => { const x = appender.debug(arg); listener(LOG_DEBUG, arg); return x },
+    info:   arg => { const x = appender.info (arg); listener(LOG_INFO , arg); return x },
+    warn:   arg => { const x = appender.warn (arg); listener(LOG_WARN , arg); return x },
+    error:  arg => { const x = appender.error(arg); listener(LOG_ERROR, arg); return x },
+    fatal:  arg => { const x = appender.fatal(arg); listener(LOG_FATAL, arg); return x },
+    reset:  ()  => { const x = appender.reset();  listener(LOG_NOTHING); return x }, // we notify via log nothing to indicate the reset
+    getValue: appender.getValue
   };
 };
-
-/**
- *
- * @type { IObservable }
- */
-let logObservable = Observable(emptyStack);
-
-/**
- * This appender returns an observable containing a stack
- * @function
- * @returns { IObservable }
- */
-const getValue = () => logObservable;
-
-/**
- * @caution Since calling "reset" provides a new observable, you have to register yourself again on the new observable.
- * @return { IObservable } - a new observable
- */
-// todo dk: who will be notified about which events?
-// what about unsubscription?
-const reset = () => {
-  const lastValue = logObservable.getValue();
-  logObservable.setValue(emptyStack);
-  // copy observable
-  return Observable(lastValue); // todo dk: what about the old observable? Memory leak?
-};
-
-/**
- * Appends the next log message to the stack.
- * @type {
- *          (type: PrioritySupplier)  =>
- *          (number)                  =>
- *          (onOverflow: *)           =>
- *          (msg: String)             =>
- *          ChurchBooleanType
- *      }
- */
-const appenderCallback = type => limit => onOverflow => msg =>
-  LazyIf(full(limit))
-    // if the stack is full, call the overflow function and add the new value afterwards.
-    (() => append(type)(msg)(limit)(onOverflow))
-    // in any other case just append the new message.
-    (() => append(type)(msg)(limit)(id));
-
-
-/**
- * the function to append trace logs in this application
- */
-const trace = appenderCallback(LOG_TRACE);
-
-/**
- * the function to append debug logs in this application
- */
-const debug = appenderCallback(LOG_DEBUG);
-
-/**
- * the function to append info logs in this application
- */
-const info = appenderCallback(LOG_INFO);
-
-/**
- * the function to append warn logs in this application
- */
-const warn = appenderCallback(LOG_WARN);
-
-/**
- * the function to append error logs in this application
- */
-const error = appenderCallback(LOG_ERROR);
-
-/**
- * the function to append fatal logs in this application
- */
-const fatal = appenderCallback(LOG_FATAL);
-
-
-/**
- * Returns {@link T} if the stack equals the limit.
- * @param { Number } limit
- * @returns ChurchBooleanType
- * @private
- */
-const full = limit =>
-  churchBool( limit === jsNum(size(logObservable.getValue()))); // todo dk: be more defensive
-
-/**
- * Appends the given message to the stack.
- * If the stack size equals the param limit, the stack will be evicted using the defined eviction strategy.
- * @type {
- *        (type: PrioritySupplier) =>
- *        (msg: String) =>
- *        (limit: Number) =>
- *        (evictionStrategy: UnaryOperatorType<IObservable>) =>
- *        ChurchBooleanType
- * }
- */
-const append = type => msg => limit => evictionStrategy => {
-  // evict the stack using the given evictionStrategy
-  logObservable = /** @type {IObservable} */ evictionStrategy(logObservable);
-  LazyIf(full(limit))
-    ( () => {
-      // if the stack is full, despite using the set eviction strategy, use the default eviction strategy to make space.
-      logObservable = DEFAULT_CACHE_EVICTION_STRATEGY(logObservable);
-      logObservable.setValue(createNewStack(LOG_ERROR)(OVERFLOW_LOG_MESSAGE));
-      logObservable.setValue(createNewStack(type)(msg));
-    })
-    (() => logObservable.setValue(createNewStack(type)(msg)));
-  return /** @type {ChurchBooleanType} */ T;
-};
-
-/**
- * creates a new stack on top of the observable-stack
- * @type {
- *   (type: PrioritySupplier) =>
- *   (msg: String) =>
- *   stack
- * }
- */
-const createNewStack = type => msg =>
-  push(logObservable.getValue())(Pair(type)(msg));
