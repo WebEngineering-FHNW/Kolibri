655fa5bda10181684968343c33c8c3e9666fae9b
Tobias Wyss <towyss@gmail.com>
2023-03-21 09:15:07 +0100

    test(iterator): add performance test file

--- contrib/wild_wyss/src/iterator/performanceTest.js
+++ contrib/wild_wyss/src/iterator/performanceTest.js
@@ -1,22 +1,37 @@
-import {TestSuite} from "../../../../docs/src/kolibri/util/test.js";
-import {emptyIterator} from "./iterator.js";
-import {cons, take, map} from "./intermediateOperations.js";
-import {reduce$} from "./terminalOperations.js";
-import {Range} from "../range/range.js";
+import { TestSuite }        from "../../../../docs/src/kolibri/util/test.js";
+import { emptyIterator }    from "./iterator.js";
+import { cons }             from "./intermediateOperations.js";
+import { reduce$ }          from "./terminalOperations.js";
+import { IteratorBuilder }  from "./iteratorBuilder.js";
 
-const iteratorSuite = TestSuite("PerformanceTest");
-iteratorSuite.add("test many cons", assert => {
-  let it = emptyIterator;
-  const limit = 12000;
-  for (let i = 0; i < limit; i++) {
-    it = cons(i)(it);
-  }
-  console.log(reduce$( (acc, cur) => acc + cur, 0)(it));
+const performanceSuite = TestSuite("PerformanceTest");
+
+performanceSuite.add("compare IteratorBuilder and cons: IteratorBuilder 1000x faster", assert => {
+  const limit = 5000;
+
+  const builderTime = measure(() => {
+    const builder = IteratorBuilder();
+    for (let i = 0; i < limit; i++) { builder.prepend(i); }
+    const built = builder.build();
+    return reduce$( (acc, cur) => acc + cur, 0)(built);
+  }, "builder");
+
+  const consTime = measure(() => {
+    let consed = emptyIterator;
+    for (let i = 0; i < limit; i++) { consed = cons(i)(consed)}
+    return reduce$( (acc, cur) => acc + cur, 0)(consed);
+  }, "Cons");
+
+  assert.is(consTime / 1000 > builderTime, true);
+
 });
 
-iteratorSuite.add("test reduce big range", assert => {
- let it = Range(120_000_000);
- it = map(el => el * 2)(it);
-  console.log("with range: " + reduce$( (acc, cur) => acc + cur, 0)(it));
-});
-iteratorSuite.run();
+const measure = (workLoad, name) => {
+  const now = performance.now();
+  const result = workLoad();
+  const then = performance.now();
+  const runningTime = then - now;
+  console.log(name, " took " , runningTime, "ms and produced following result: ", result);
+  return runningTime;
+};
+performanceSuite.run();
