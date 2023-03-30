7f852af92774be45d4e744871153b4a4d1e8802d
Tobias Wyss <towyss@gmail.com>
2023-03-30 14:43:21 +0200

    feat(iterator): add scale factor to fibonacci example

--- contrib/wild_wyss/src/iterator/example/fibonacci/fibonacci.js
+++ contrib/wild_wyss/src/iterator/example/fibonacci/fibonacci.js
@@ -5,28 +5,28 @@ import { iteratorProjector }  from "../../projector/iteratorProjector.js";
 import {FibonacciIterator} from "../../iterator.js";
 
 
-const fibonacciView = (rootElement, amount) => {
+const fibonacciView = (rootElement, amount, scaleFactor) => {
   const iti     = _.take(amount)(FibonacciIterator());
   const iti2    = _.cons(0)(FibonacciIterator());
   const values  = _.zipWith((a, b) => ({current: a, last: b}))(iti)(iti2);
   const indexed = _.zipWith((a, b) => ({index: b, current: a.current, last: a.last}))(values)(Range(100));
 
-  const elements = (iteratorProjector(indexed)(fibonacciProjector(10))).children;
+  const elements = (iteratorProjector(indexed)(fibonacciProjector(scaleFactor))).children;
 
   rootElement.append(...elements);
 };
 
-const fibonacciProjector = scaleSactor => ({index, current, last}) => {
+const fibonacciProjector = scaleFactor => ({index, current, last}) => {
   const [div] = dom(`<div>${current}</div>`);
-  let top = last * scaleSactor;
+  let top = last * scaleFactor;
   let left = 0;
   if (index % 2 === 0) {
     top = 0;
-    left = last * scaleSactor;
+    left = last * scaleFactor;
   }
   div.style.border    = `1px solid black`;
-  div.style.height    = `${current * scaleSactor}px`;
-  div.style.width     = `${current * scaleSactor}px`;
+  div.style.height    = `${current * scaleFactor}px`;
+  div.style.width     = `${current * scaleFactor}px`;
   div.style.position  = 'absolute';
   div.style.top       = `${top}px`;
   div.style.left      = `${left}px`;
@@ -35,10 +35,21 @@ const fibonacciProjector = scaleSactor => ({index, current, last}) => {
 
 const rootElement   = document.getElementById("root");
 const amountInput   = document.getElementById("amount");
-const amount        = 5;
+const scaleInput    = document.getElementById("scale");
+let amount        = 5;
+let scaleFactor   = 10;
+
+
 amountInput.value  = amount;
 amountInput.onchange = () => {
   rootElement.innerHTML = '';
-  fibonacciView(rootElement, Number(amountInput.value));
+  amount = Number(amountInput.value);
+  fibonacciView(rootElement, amount, scaleFactor);
 };
-fibonacciView(rootElement, amount);
+
+scaleInput.onchange = () => {
+  rootElement.innerHTML = '';
+  scaleFactor = Number(scaleInput.value);
+  fibonacciView(rootElement, amount, scaleFactor);
+};
+fibonacciView(rootElement, amount, scaleFactor);
