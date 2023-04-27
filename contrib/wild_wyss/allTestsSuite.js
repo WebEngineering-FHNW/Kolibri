ee37f35eee98412e55d581d15a89ff4eb6ffbb0d
Andri Wild <andri.wild@students.fhnw.ch>
2023-04-27 11:49:37 +0200

    test(stdlib): add stdlibTest file to allTests

--- contrib/wild_wyss/allTestsSuite.js
+++ contrib/wild_wyss/allTestsSuite.js
@@ -3,23 +3,26 @@ import { versionInfo } from "../../docs/src/kolibri/version.js";
 
 // logger
 import "./src/logger/loggerTest.js";
-import "./src/logger/appender/arrayAppenderTest.js"
-import "./src/logger/appender/countAppenderTest.js"
-import "./src/logger/appender/consoleAppenderTest.js"
-import "./src/logger/appender/observableAppenderTest.js"
-import "./src/logger/logUi/logUiControllerTest.js"
+import "./src/logger/appender/arrayAppenderTest.js";
+import "./src/logger/appender/countAppenderTest.js";
+import "./src/logger/appender/consoleAppenderTest.js";
+import "./src/logger/appender/observableAppenderTest.js";
+import "./src/logger/logUi/logUiControllerTest.js";
 
 // iterator
-import "./src/iterator/constructors/constructorsTest.js"
-import "./src/iterator/operators/operatorsTest.js"
-import "./src/iterator/terminalOperations/terminalOperationsTest.js"
-import "./src/iterator/iteratorBuilderTest.js"
+import "./src/iterator/constructors/constructorsTest.js";
+import "./src/iterator/operators/operatorsTest.js";
+import "./src/iterator/terminalOperations/terminalOperationsTest.js";
+import "./src/iterator/iteratorBuilderTest.js";
 
 // range
-import "./src/range/rangeTest.js"
+import "./src/range/rangeTest.js";
 
 // focusring
-import "./src/focusring/focusRingTest.js"
+import "./src/focusring/focusRingTest.js";
+
+// stdlib
+import "./src/stdlib/stdlibTest.js";
 
 total.onChange(value => document.getElementById('grossTotal').textContent = "" + value + " tests done.");
 
