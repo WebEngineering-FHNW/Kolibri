2bd487084d4516cfd49473775d307846c789a6b5
Andri Wild <andri.wild@students.fhnw.ch>
2023-05-10 10:19:53 +0200

    refactor(iterator): implement mconcat lazily

--- contrib/wild_wyss/src/iterator/operators/mconcat.js
+++ contrib/wild_wyss/src/iterator/operators/mconcat.js
@@ -1,27 +1,52 @@
-import { reduce$, ConcatIterator, nil  } from "../iterator.js";
+import { createIterator, nextOf } from "../util/util.js";
 
 export { mconcat }
 
 /**
+ * Flatten an {@link IteratorMonadType} of {@link IteratorMonadType IteratorMonadType's}.
+ *
+ * _Note:_
+ * Iterator passed to {@link mconcat} is processed lazily.
+ * Therefore, its sub iterators are not copied until these are processed.
+ * Otherwise, the passed iterator must be processed eagerly and couldn't contain infinite iterators.
+ *
  * @function
  * @template _T_
- * @pure both iterators will be copied defensively
- * @param { IteratorType<IteratorType<_T_>> } iterator -
- * @returns IteratorType<_T_>
+ * @pure iterator and sub iterators will be copied defensively
+ * @param { IteratorMonadType<IteratorMonadType<_T_>> } iterator -
+ * @returns IteratorMonadType<_T_>
  * @example
- * const iterators = ArrayIterator([
- *   Range(2),
- *   Range(2),
- *   Range(2),
- * ]);
- * const result = mconcat(iterators);
+ * const ranges = map(x => Range(x))(Range(2));
+ * const result = mconcat(ranges);
  * console.log(...result);
- * // prints: 0, 1, 2, 0, 1, 2, 0, 1, 2
+ * // prints: 0, 0, 1, 0, 1, 2
  */
-const mconcat = iterator =>
-  /**
-   * @template _T_
-   * @type { IteratorType<_T_> }
-   */
-  reduce$((acc, cur) => ConcatIterator(acc)(cur), nil)(iterator);
+const mconcat = iterator => {
 
+  const mconcatFactory = (outer, current = undefined) => {
+
+    const next = () => {
+      while (true) {
+        if (current === undefined) {
+          // if there is no current, get the next sub iterator of the outer iterator
+          const nextOfOuter = nextOf(outer);
+          if (nextOfOuter.done) return nextOfOuter;
+          current = nextOfOuter.value.copy();
+        }
+        // grab next iterator value from sub iterator until it is done
+        const nextOfCurrent = nextOf(current);
+        if (!nextOfCurrent.done) return nextOfCurrent;
+        current = undefined;
+      }
+    };
+
+    const copy = () => {
+      const safeCopyCurrent = current === undefined ? current : current.copy();
+      return mconcatFactory(outer.copy(), safeCopyCurrent);
+    };
+
+    return createIterator(next, copy);
+  };
+  return mconcatFactory(iterator.copy())
+
+};
