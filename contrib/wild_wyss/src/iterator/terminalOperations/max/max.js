5039a6e5fb17d52ed2677ccf4fd74ae75094675c
Tobias Wyss <towyss@gmail.com>
2023-05-12 08:54:58 +0200

    refactor(iterator): review min$ & max$

--- contrib/wild_wyss/src/iterator/terminalOperations/max/max.js
+++ contrib/wild_wyss/src/iterator/terminalOperations/max/max.js
@@ -1,26 +1,24 @@
-import { nextOf }                 from "../../util/util.js";
-import { isEmpty }                from "../isEmpty/isEmpty.js";
-import { ILLEGAL_ARGUMENT_EMPTY_ITERATOR } from "../../util/errorMessages.js";
+import { nextOf }                           from "../../util/util.js";
+import { ILLEGAL_ARGUMENT_EMPTY_ITERATOR }  from "../../util/errorMessages.js";
 
 export { max$ }
 
 /**
- *  Returns the largest element of a non-empty {@link IteratorType}.
- *  Passing an empty iterator to {@link max$} will throw an error.
+ *  Returns the largest element of a **non-empty** {@link IteratorType}.
  *
  *  _Note_:
  *  To determine the largest element, a comparator function is used.
  *  This function compares two elements by default with the `< (LT)` operator.
- *  Where on the left side is the current largest element when processing the iterator..
- *  If needed, the comparator can also be passed as a second argument to {@link max$}
+ *  Where on the left side is the current largest element when processing the iterator.
+ *  If needed, a different comparator can also be passed as a second argument to {@link max$}
  *  and will then be used to determine the largest element.
  *
  * @function
  * @template _T_
- * @template _U_
- * @param { IteratorType<_T_> } iterator
- * @param { BiPredicate<_T_, _U_> } comparator
+ * @param { IteratorType<_T_> }     iterator     - a non-empty finite iterator
+ * @param { BiPredicate<_T_, _T_> } [comparator] - an optional comparing function which returns true if the second argument is larger than the first
  * @returns _T_
+ * @throws { Error } {@link ILLEGAL_ARGUMENT_EMPTY_ITERATOR} if the given iterator is empty
  * @example
  * const it = Range(3);
  * const maximum = max$(it);
@@ -28,16 +26,16 @@ export { max$ }
  * // => Logs: 3
  */
 const max$ = (iterator, comparator = (a, b) => a < b) => {
+  const inner                     = iterator.copy();
+  let { value: currentMax, done } = nextOf(inner);
 
-  if(isEmpty(iterator)) {
+  if (done) {
+    // iterator is empty, no max can be found
     throw Error(ILLEGAL_ARGUMENT_EMPTY_ITERATOR);
   }
 
-  const inner    = iterator.copy();
-  let currentMax = nextOf(inner).value;
-
   for (const elem of inner) {
-    if(comparator(currentMax, elem)) {
+    if (comparator(currentMax, elem)) {
       currentMax = elem;
     }
   }
