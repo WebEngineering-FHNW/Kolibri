4753c59cfb228d2c3a07b1631bd9ab452da2f7d1
Andri Wild <andri.wild@students.fhnw.ch>
2023-05-16 16:59:29 +0200

    feat(jinq): start to implement jinq json

--- contrib/wild_wyss/src/jinq/jinqTest.js
+++ contrib/wild_wyss/src/jinq/jinqTest.js
@@ -92,4 +92,124 @@ jinqSuite.add("test join with Nothing", assert => {
   assert.is(result, Nothing);
 
 });
+
+jinqSuite.add("object test", assert => {
+
+  /**
+   *
+   * @param obj
+   * @return {}
+   * @constructor
+   */
+  const JsonWrapper = obj => {
+
+    /**
+     *
+     * @param {}maybeObj
+     * @return {any}
+     * @constructor
+     */
+    const JsonWrapperFactory = maybeObj => {
+
+      const pure = a => JsonWrapperFactory(Just(a));
+
+      const empty = () => JsonWrapperFactory(Nothing);
+
+      const fmap = f => {
+       const result = maybeObj.and(x => {
+         const result = f(x);
+         return result ? Just(result): Nothing;
+       });
+       return JsonWrapperFactory(result);
+      };
+
+      // x => x.adress
+      // f :: _T_ -> JsonWrapper<_U_>
+      const and = f => {
+
+       const result = maybeObj.and(x => {
+         const result = f(x);
+         return result.get();
+       });
+       return JsonWrapperFactory(result);
+      };
+
+      const get = () => maybeObj;
+
+      return {
+        pure,
+        empty,
+        fmap,
+        and,
+        get,
+      }
+    };
+
+    return JsonWrapperFactory(Just(obj));
+  };
+
+
+  const sample = JSON.parse( `
+  {
+    "address": {
+      "address": "629 Debbie Drive",
+        "city": "Nashville",
+        "coordinates": {
+        "lat": 36.208114,
+          "lng": -86.58621199999999
+      },
+      "postalCode": "47076",
+        "state": "TN"
+    },
+    "department": "Marketing",
+      "name": "Blanda-O'Keefe",
+      "title": "Help Desk Operator"
+  }
+  `);
+
+  const sample2 = JSON.parse( `
+  {
+    "people": [
+    {
+      "postalCode": "47076",
+      "name": "Atoandias"
+      },
+      
+    {
+      "postalCode": "47076",
+      "name": "Tanobiri"
+      },
+    {
+      "postalCode": "37076",
+      "name": "Drk"
+      }
+      ]
+  }
+  `);
+
+
+  const wrapper =
+    from(JsonWrapper(sample))
+      .select(x => x.address)
+      .select(x => x.postalCode)
+      .result()
+      .get();
+  wrapper(x => console.log("nothing"))(x => console.log("just: " + x));
+
+  const wrapper2 =
+    from(JsonWrapper(sample))
+      .select(x => x.address)
+      .select(x => x.postalCode)
+      .join(JsonWrapper(sample2))
+      .where(tuple => {
+        console.log(tuple(fst), tuple(snd));
+       return  tuple(fst) === tuple(snd).postalCode
+      })
+      .result()
+      .get();
+  wrapper2(x => console.log("nothing"))(x => console.log("just: " + x))
+
+});
+
+
 jinqSuite.run();
