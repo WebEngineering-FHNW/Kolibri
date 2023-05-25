a203be42f0db410e2750e1cbefa4db53a9a4c881
Tobias Wyss <towyss@gmail.com>
2023-05-25 10:01:30 +0200

    test(iterator): refactor concat test

--- contrib/wild_wyss/src/iterator/operators/concat/concatTest.js
+++ contrib/wild_wyss/src/iterator/operators/concat/concatTest.js
@@ -22,32 +22,46 @@ addToTestingTable(testSuite)(
   })
 );
 
-testSuite.add("test purity: concat", assert => {
+testSuite.add("test purity: concat - both sub iterators untouched.", assert => {
+  // Given
   const it1 = newIterator(4);
   const it2 = newIterator(2);
   const concatIterator = concat(it1)(it2);
+
+  // When
   for (const _ of concatIterator) { /* Exhausting */ }
+
+  // Then
   assert.isTrue(arrayEq([0,1,2,3,4])([...it1]));
   assert.isTrue(arrayEq([0,1,2])    ([...it2]));
 });
 
-testSuite.add("test left/right neutrality: concat", assert => {
-  const left =  concat(nil)(newIterator(4));
-  const right = concat(newIterator(4))(nil);
+testSuite.add("test left/right identity: concat", assert => {
+  // Given
+  const base = newIterator(4);
+  // When
+  const left =  concat(nil)(base.copy());
+  const right = concat(base)(nil);
+
+  // Then
   const expected = [0,1,2,3,4];
   assert.isTrue(arrayEq(expected)([...right]));
   assert.is(arrayEq(expected)([...left]),  true);
 });
 
 testSuite.add("test left/right associativity: concat", assert => {
+  // When
   const left  = concat(concat(newIterator(2))(newIterator(1)))(newIterator(3));
   const right = concat(newIterator(2))(concat(newIterator(1))(newIterator(3)));
+
+  // Then
   const expected = [0,1,2,0,1,0,1,2,3];
   assert.isTrue(arrayEq(expected)([...right]));
   assert.is(arrayEq(expected)([...left]),  true);
 });
 
 testSuite.add("test concat with infinity: concat", assert => {
+  // Given
   let called  = false;
   let counter = 0;
 
@@ -55,14 +69,20 @@ testSuite.add("test concat with infinity: concat", assert => {
   const iteratorWithSideEffect = Iterator(false, _ => called = true, _ => false);
   const concatenated           = concat(endless)(iteratorWithSideEffect);
 
+  // When
   for (const _ of concatenated) {
     if (counter++ > 10) break; // consume a few elements
   }
+
+  // Then
   assert.is(called, false);
 });
 
 testSuite.add("test empty: concat", assert => {
+  // When
   const concatenated = concat(nil)(nil);
+
+  // Then
   assert.is(arrayEq([])([...concatenated]),  true);
 });
 
