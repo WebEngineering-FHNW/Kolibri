774e0b7bfc2953405a6bf4e1cf5afefdd25a873a
Tobias Wyss <towyss@gmail.com>
2023-05-25 09:51:29 +0200

    test(iterator): add further tests for pipe

--- contrib/wild_wyss/src/iterator/operators/pipe/pipeTest.js
+++ contrib/wild_wyss/src/iterator/operators/pipe/pipeTest.js
@@ -8,6 +8,8 @@ import {
   UPPER_ITERATOR_BOUNDARY
 }                            from "../../util/testUtil.js";
 import {rejectAll} from "../rejectAll/rejectAll.js";
+import {reduce$} from "../../terminalOperations/reduce/reduce.js";
+import {arrayEq} from "../../../../../../docs/src/kolibri/util/arrayFunctions.js";
 
 const testSuite = TestSuite("Iterator: Operation pipe");
 testSuite.run();
@@ -21,5 +23,33 @@ addToTestingTable(testSuite)(
     expected:   [0, 2, 4],
   })
 );
+
+testSuite.add("Test pipe with terminal operation", assert => {
+  // Given
+  const base    = newIterator(5);
+  const mapper  = map        (x => 2*x);
+  const reducer = reduce$    ((acc, cur) => acc + cur, 0);
+
+  // When
+  const res = pipe(
+    mapper, // [0, 2, 4, 6, 8, 10]
+    reducer // 0 + 0 + 2 + 4 + 6 + 8 + 10
+  )(base); // [0, 1, 2, 3, 4, 5]
+
+  //Then
+  assert.is(res, 30);
+});
+
+
+testSuite.add("Test with no operations", assert => {
+  // Given
+  const base = newIterator(5);
+
+  // When
+  const res = pipe()(base);
+
+  //Then
+  assert.isTrue(arrayEq([...base])([...res]))
+});
 
 testSuite.run();
