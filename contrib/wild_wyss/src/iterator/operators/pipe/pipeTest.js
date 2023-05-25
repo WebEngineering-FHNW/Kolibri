079afd8118e8b4d41aa583245c3543191b90c801
Tobias Wyss <towyss@gmail.com>
2023-05-25 09:38:23 +0200

    test(iterator): add tests for pipe

--- contrib/wild_wyss/src/iterator/operators/pipe/pipeTest.js
+++ contrib/wild_wyss/src/iterator/operators/pipe/pipeTest.js
@@ -1,6 +1,25 @@
-import {TestSuite} from "../../../test/test.js";
+import { TestSuite         } from "../../../test/test.js";
+import { addToTestingTable } from "../../util/testingTable.js";
+import { pipe              } from "./pipe.js";
+import { map               } from "../map/map.js";
+import {
+  createTestConfig,
+  newIterator,
+  UPPER_ITERATOR_BOUNDARY
+}                            from "../../util/testUtil.js";
+import {rejectAll} from "../rejectAll/rejectAll.js";
 
 const testSuite = TestSuite("Iterator: Operation pipe");
 testSuite.run();
 
-// TODO
+addToTestingTable(testSuite)(
+  createTestConfig({
+    name:       "pipe",
+    iterator:   () => newIterator(UPPER_ITERATOR_BOUNDARY),
+    operation:  ops => pipe(...ops),
+    param:      [ map(x => 2*x), rejectAll(x => x > 4) ],
+    expected:   [0, 2, 4]
+  })
+);
+
+testSuite.run();
