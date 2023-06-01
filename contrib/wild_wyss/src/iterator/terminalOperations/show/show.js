0d3ac4503554dc1c64014f78e62a6b6bae6df6aa
Tobias Wyss <towyss@gmail.com>
2023-06-01 16:09:40 +0200

    docs(iterator): add jsdoc to show

--- contrib/wild_wyss/src/iterator/terminalOperations/show/show.js
+++ contrib/wild_wyss/src/iterator/terminalOperations/show/show.js
@@ -2,10 +2,19 @@ import { pipe, reduce$, take } from "../../iterator.js";
 
 export { show }
 
-const show = (iterator, maxValues = 50) =>
+/**
+ * Transforms the passed {@link Iterable} to a {@link String}.
+ *
+ * @template _T_
+ * @pure the passed iterable will not be changed.
+ * @param { Iterable<_T_> } iterable
+ * @param { number }        [maxValues=50] - the amount of elements that should be printed at most
+ * @returns { String }
+ */
+const show = (iterable, maxValues = 50) =>
   "[" +
   pipe(
     take(maxValues),
     reduce$((acc, cur) => acc === "" ? cur : `${acc},${String(cur)}`, ""),
-  )(iterator)
+  )(iterable)
   + "]";
--- contrib/wild_wyss/src/iterator/terminalOperations/show/showTest.js
+++ contrib/wild_wyss/src/iterator/terminalOperations/show/showTest.js
@@ -25,26 +25,44 @@ addToTestingTable(testSuite)(
 );
 
 testSuite.add("test boundary value", assert => {
+  // Given
   const it = Range(0);
+
+  // When
   const result = show(it);
+
+  // Then
   assert.is(result, "[0]");
 });
 
 testSuite.add("test given output length", assert => {
+  // Given
   const it     = Range(100);
+
+  // When
   const result = show(it, 2);
+
+  // Then
   assert.is(result, "[0,1]");
 });
 
 testSuite.add("test equality of show and toString", assert => {
+  // Given
   const it     = Range(10);
+
+  // When
   const result = show(it);
+
+  // Then
   assert.is(result, it.toString());
 });
 
 
 testSuite.add("test exceed default output length (50)", assert => {
+  // Given
   const it     = Range(100);
+
+  // When
   const result = show(it);
   /**
    * 2  [braces]
@@ -53,12 +71,19 @@ testSuite.add("test exceed default output length (50)", assert => {
    * 40 [10-49] ( x2 )
    */
   const outputLength = 2 + 49 + 10 + 2 * 40;
+
+  // Then
   assert.is(result.length, outputLength);
 });
 
 testSuite.add("test show of an iterator of iterators", assert => {
+  // Given
   const it     = ArrayIterator([Range(1), Range(2), Range(3)]);
+
+  // When
   const result = show(it);
+
+  // Then
   assert.is(result, "[[0,1],[0,1,2],[0,1,2,3]]");
 });
 
