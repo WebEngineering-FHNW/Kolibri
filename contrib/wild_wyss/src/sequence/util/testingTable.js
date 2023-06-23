21c3aee0bfec813f06e63b89bc5a3b345fc2a096
Andri Wild <andri.wild@students.fhnw.ch>
2023-06-23 13:48:33 +0200

    refactor(iterator): rename testingFunctions to testingTable

--- contrib/wild_wyss/src/sequence/util/testingTable.js
+++ contrib/wild_wyss/src/sequence/util/testingTable.js
@@ -42,13 +42,13 @@ const TESTS = {
 };
 
 
-const testingFunctions = [];
-testingFunctions.push({ name: TESTS.TEST_SIMPLE                  , test: testSimple});
-testingFunctions.push({ name: TESTS.TEST_PURITY                  , test: testPurity});
-testingFunctions.push({ name: TESTS.TEST_CB_NOT_CALLED_AFTER_DONE, test: testCBNotCalledAfterDone});
-testingFunctions.push({ name: TESTS.TEST_PROTOTYPE               , test: testPrototype});
-testingFunctions.push({ name: TESTS.TEST_INVARIANTS              , test: testInvariants});
-testingFunctions.push({ name: TESTS.TEST_ITERATE_MULTIPLE_TIMES  , test: testIterateMultipleTimes});
+const testingTable = [];
+testingTable.push({ name: TESTS.TEST_SIMPLE                  , test: testSimple});
+testingTable.push({ name: TESTS.TEST_PURITY                  , test: testPurity});
+testingTable.push({ name: TESTS.TEST_CB_NOT_CALLED_AFTER_DONE, test: testCBNotCalledAfterDone});
+testingTable.push({ name: TESTS.TEST_PROTOTYPE               , test: testPrototype});
+testingTable.push({ name: TESTS.TEST_INVARIANTS              , test: testInvariants});
+testingTable.push({ name: TESTS.TEST_ITERATE_MULTIPLE_TIMES  , test: testIterateMultipleTimes});
 
 /**
  * @type {
@@ -60,7 +60,7 @@ testingFunctions.push({ name: TESTS.TEST_ITERATE_MULTIPLE_TIMES  , test: testIte
 const addToTestingTable = testSuite => config => {
   const { excludedTests } = config;
 
-  testingFunctions
+  testingTable
     .filter (({ name })        => !excludedTests.includes(name))
     .forEach(({ name, test })  => testSuite.add(`${name}: ${config.name}`, test(config)));
 };
