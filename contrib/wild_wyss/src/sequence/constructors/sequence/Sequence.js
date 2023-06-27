8f5d2d1579000c3b8c79868a0dfa0e8d456df245
Andri Wild <andri.wild@students.fhnw.ch>
2023-06-27 13:04:52 +0200

    refactor(iterator): rename untilFunction of sequence to whileFunction

--- contrib/wild_wyss/src/sequence/constructors/sequence/Sequence.js
+++ contrib/wild_wyss/src/sequence/constructors/sequence/Sequence.js
@@ -4,32 +4,33 @@ import { bind, map, PureSequence, show, eq$, pipe } from "../../sequence.js"
 export { Sequence, SequencePrototype }
 
 /**
- * The `incrementFunction` should change the value (make progress) in a way that the `untilFunction` function can
+ * The `incrementFunction` should change the value (make progress) in a way that the `whileFunction` function can
  * recognize the end of the sequence.
  *
  * Contract:
- * - `incrementFunction` & `untilFunction` should not refer to any mutable state variable (because of side effect) in
+ * - `incrementFunction` & `whileFunction` should not refer to any mutable state variable (because of side effect) in
  *   the closure.
  *
  * @constructor
- * @pure if `untilFunction` & `incrementFunction` are pure
+ * @pure if `whileFunction` & `incrementFunction` are pure
  * @template _T_
  * @param   { _T_ }               start             - the first value to be returned by this sequence
- * @param   { (_T_) => Boolean }  untilFunction     - returns false if the iteration should stop
+ * @param   { (_T_) => Boolean }  whileFunction     - returns false if the iteration should stop
  * @param   { (_T_) => _T_ }      incrementFunction - calculates the next value based on the previous
  * @returns { SequenceType<_T_> }
  *
  * @example
  * const start      = 0;
- * const untilF     = x => x < 3;
+ * const whileF     = x => x < 3;
  * const incrementF = x => x + 1;
- * const sequence   = Sequence(start, untilF, incrementF);
+ * const sequence   = Sequence(start, whileF, incrementF);
  *
  * console.log(...sequence);
  * // => Logs '0, 1, 2'
  */
 
-const Sequence = (start, untilFunction, incrementFunction) => {
+// whileFunction
+const Sequence = (start, whileFunction, incrementFunction) => {
 
   const iterator = () => {
     let value = start;
@@ -40,7 +41,7 @@ const Sequence = (start, untilFunction, incrementFunction) => {
      */
     const next = () => {
       const current = value;
-      const done = !untilFunction(current);
+      const done = !whileFunction(current);
       if (!done) value = incrementFunction(value);
       return { done, value: current };
     };
