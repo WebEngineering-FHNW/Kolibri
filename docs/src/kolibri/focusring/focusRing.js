5bb8a7aa3d20554600672024954b511e3ea3cbeb
Dierk Koenig <dierk.koenig@canoo.com>
2023-09-11 09:46:17 +0200

    housekeeping: adding toSeq to sequence module, clarifying the focusRing

--- docs/src/kolibri/focusring/focusRing.js
+++ docs/src/kolibri/focusring/focusRing.js
@@ -1,12 +1,4 @@
-import {
-  nil,
-  reverse$,
-  cons,
-  drop,
-  eq$,
-  head,
-  isEmpty,
-} from "../sequence/sequence.js"
+import {cons, drop, head, isEmpty, nil, reverse$, Seq, toSeq} from "../sequence/sequence.js";
 
 export { FocusRing }
 
@@ -15,42 +7,35 @@ export { FocusRing }
  *
  * @constructor
  * @template _T_
- * @param   { !Iterable<_T_> } nonEmptyIterable - A finite {@link Iterable} which has at least one element.
+ * @param   { !Iterable<_T_> } nonEmptyIterable - A finite {@link Iterable} which has **at least one element**.
  * @returns { FocusRingType<_T_> }
  */
-const FocusRing = nonEmptyIterable => FocusRingInternal(
-  nil,
-  nonEmptyIterable
-);
+const FocusRing = nonEmptyIterable => FocusRingInternal(nil, toSeq(nonEmptyIterable));
 
 /**
  * Constructs a new immutable focus ring using the given {@link Iterable}.
  *
  * @constructor
  * @template _T_
- * @param   { !Iterable<_T_> } pre  - a finite iterable
- * @param   { !Iterable<_T_> } post - a finite iterable which has at least one element, it's head is the focus.
+ * @param   { SequenceType<_T_> } pre  - a finite sequence
+ * @param   { SequenceType<_T_> } post - a finite sequence which has at least one element, it's head is the focus.
  * @returns { FocusRingType<_T_> }
  */
 const FocusRingInternal = (pre, post) => {
-  const focus = () => head(post);
+
+  const focus = () => post.head();
 
   const right = () => {
-    const currentFocus = head(post);
-    const modifiedPost = drop(1)(post);
+    const oldFocus = post.head();
+    const tailPost = post.drop(1);
 
-    if (eq$(modifiedPost)(nil)) {
-      if (eq$(pre)(nil)) {
-        // do nothing when only one element in list
-        return FocusRingInternal(pre, post);
-      }
-      return FocusRingInternal(
-        [currentFocus],
-        reverse$(pre)
-      );
+    if (pre.isEmpty() && tailPost.isEmpty()) {                        // only one element in list (the focus): do nothing
+      return FocusRingInternal(pre, post);
     }
-    const modifiedPre = cons(currentFocus)(pre);
-    return FocusRingInternal(modifiedPre, modifiedPost);
+    if (tailPost.isEmpty()) {                                        // end of list reached: cycle to close the ring
+      return FocusRingInternal(Seq(oldFocus), pre.reverse$());
+    }
+    return FocusRingInternal(cons(oldFocus)(pre), tailPost);        // normal case: move focus to the right
   };
 
   const left = () => {
@@ -64,7 +49,7 @@ const FocusRingInternal = (pre, post) => {
 
     // remove head from pre and add it to post
     const headPre = head(modifiedPre);
-    modifiedPre   = drop(1)(modifiedPre);
+    modifiedPre   = drop(1)(modifiedPre);          // todo dk: destructive update is not so nice
     modifiedPost  = cons(headPre)(modifiedPost);
 
     return FocusRingInternal(modifiedPre, modifiedPost);
