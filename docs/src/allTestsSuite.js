fca094d097f579969cda290deca770ea6ce3d0fb
Dierk Koenig <dierk.koenig@canoo.com>
2023-09-18 15:52:44 +0200

    refactoring: remove duplication in test suite.

--- docs/src/allTestsSuite.js
+++ docs/src/allTestsSuite.js
@@ -1,10 +1,3 @@
 
-import { total }      from "./kolibri/util/test.js";
-import { versionInfo} from "./kolibri/version.js";
-
 import '../src/examples/allExampleTestsSuite.js';
 import '../src/kolibri/allKolibriTestsSuite.js';
-
-total.onChange( value => document.getElementById('grossTotal').textContent = "" + value + " tests done.");
-
-document.querySelector("footer").textContent = "Built with Kolibri " + versionInfo;
--- docs/src/manualTestsSuite.js
+++ docs/src/manualTestsSuite.js
@@ -3,14 +3,8 @@
  * These are tests that we don't want to run all the time but only occasionally because they
  * take extra time, clutter the console, or need manual inspection of the result.
  */
-import { total }       from "./kolibri/util/test.js";
-import { versionInfo } from "./kolibri/version.js";
 
 import "./kolibri/sequence/performanceTest.js"     // time dependent
 import "./kolibri/logger/appender/consoleAppenderTest.js"; // writes to console
 import "./kolibri/logger/appender/countAppenderTest.js";   // writes to console
 import "./kolibri/logger/loggingUi/loggingUiControllerTest.js"; // messen up the output when tests fail. keep last.
-
-total.onChange( value => document.getElementById('grossTotal').textContent = "" + value + " tests done.");
-
-document.querySelector("footer").textContent = "Built with Kolibri " + versionInfo;
