ec227419fcd03583c1abab670469c88aca807032
Dierk Koenig <dierk.koenig@canoo.com>
2023-10-07 20:42:09 +0200

    Kolibri-15 remove the research parts that are not needed for integration

--- contrib/p6_schnidrig_altermatt/research/oop-in-javascript/interfaces/kolibri-version/TestInterfaces.js
+++
@@ -1,8 +0,0 @@
-import { Animal } from "./IAnimal.js";
-import { Monkey } from "./MonkeyImpl.js";
-
-const monkey = Animal(Monkey());
-monkey.eat();
-monkey.move();
-monkey.talk('uga uga');
-monkey.die();
