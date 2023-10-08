1bd0722f1be8b8d0f167d16d3e51414525af460c
Dierk Koenig <dierk.koenig@canoo.com>
2023-10-08 13:22:57 +0200

    Kolibri-15 remove kolibri artefacts from contribution

--- contrib/p6_schnidrig_altermatt/prototype/_examples/allExampleTestsSuite.js
+++
@@ -1,6 +0,0 @@
-
-import "./simpleForm/simpleFormViewTest.js"
-import "./workday/dayControllerTest.js"
-import "./workweek/weekControllerTest.js"
-import "./workweek/simpleWeekProjectorTest.js"
-import "./person/personTest.js"
--- contrib/p6_schnidrig_altermatt/prototype/_kolibri/allKolibriTestsSuite.js
+++
@@ -1,7 +0,0 @@
-
-import "./stdlibTest.js";
-import "./versionTest.js";
-import "./observableTest.js";
-import "./presentationModelTest.js";
-import "../navigation/navigationControllerTest.js"
-import "../pages/pageControllerTest.js"
