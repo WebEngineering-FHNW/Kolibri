23bb2146b7bd1691522c6c5ee29f7b3be83697e6
Dierk Koenig <dierk.koenig@canoo.com>
2024-01-29 16:24:43 +0100

    navigation: rename dashboardNavigationProjector to sideNavigationProjector

--- docs/src/examples/navigation/basic-nav-app.js
+++ docs/src/examples/navigation/basic-nav-app.js
@@ -1,5 +1,5 @@
 import {NavigationController}                                 from "../../kolibri/navigation/navigationController.js";
-import {DashboardRefinedProjector}                            from "../../kolibri/navigation/projector/dashboard/dashboardNavigationProjector.js";
+import {SideNavigationProjector}                            from "../../kolibri/navigation/projector/sideNavigation/sideNavigationProjector.js";
 import {registerSiteMap, resourceBaseURI, setResourceBaseURI} from "../../kolibri/navigation/applicationConfig.js";
 
 // where all the resources reside relative to the home URL.
@@ -10,7 +10,7 @@ navigationController.setWebsiteLogo(resourceBaseURI + 'img/logo/logo.svg');
 navigationController.setWebsiteName('Basic Navigation');
 
 // this must come first since the nav registrations triggers the repaint
-DashboardRefinedProjector(navigationController, document.getElementById("nav"));
+SideNavigationProjector(navigationController, document.getElementById("nav"));
 
 registerSiteMap(null /* no parent at start */, navigationController,
     [
