1efb742b627864c0d5b87bb4b2e800f4c18dbf8c
Dierk Koenig <dierk.koenig@canoo.com>
2024-11-23 00:36:26 +0100

    navigation:  experiment home/about basically working

--- docs/src/examples/navigation/pages/about.js
+++ docs/src/examples/navigation/pages/about.js
@@ -12,47 +12,44 @@ const AboutPage = () => {
         titleText,
         styleElement,
         contentElement,
-        passivate : () => console.log("about passivated"),
-        activate : () => console.log("about activated"),
     }
 };
 
-const titleText      = `About`;
+const [titleText]      = `About`;
 
 // one could also use a <link rel="stylesheet"> element
-const styleElement   = dom(`
-<style>
-    #content .about h1 {
-        margin-top: 35px;
-        color: var(--kb-hsla-primary-accent);
-    }
-    
-    #content .about .message-wrapper {
-        text-align: center;
-        padding: 16px;
-        display: flex;
-        justify-content: center;
-        color: var(--kb-hsla-primary-accent);
-    }
-    
-    #content .about button {
-        padding: 5px;
-        margin-top: 32px;
-    }    
-</style>
+const [styleElement]   = dom(`
+    <style>
+        .about {
+            h1 {
+                margin-top: 2em;
+                color:      var(--kb-hsla-primary-accent);
+            }
+            .message-wrapper {
+                text-align:         center;
+                padding:            1em;
+                display:            flex;
+                justify-content:    center;
+                color:              var(--kb-hsla-primary-accent);
+            }
+            .buttons {
+                padding:            5px;
+                margin-top:         2em;
+            }                                
+        }
+    </style>
 `);
 
-const contentElement = dom(`
-<div id="content-wrapper">
-    <h1>About</h1>
-    <div class="message-wrapper">
-        <section class="buttons">
-            <p>These "buttons" should not be styled.</p>
-            <p>This is to make sure that styles from the home page do not spill over to this page.</p>
-            <a class="btn primary glow" ${href(URI_HASH_HOME)}>Home</a>
-            <a class="btn accent  glow" ${href(URI_HASH_ABOUT)}>No Style</a>
-        </section>
+const [contentElement] = dom(`
+    <div class="about">
+        <h1>About</h1>
+        <div class="message-wrapper">
+            <section class="buttons">
+                <p>These "buttons" should not be styled.</p>
+                <p>This is to make sure that styles from the home page do not spill over to this page.</p>
+                <a class="btn primary glow" ${href(URI_HASH_HOME)}>Home</a>
+                <a class="btn accent  glow" ${href(URI_HASH_ABOUT)}>No Style</a>
+            </section>
+        </div>
     </div>
-</div>
-
 `);
