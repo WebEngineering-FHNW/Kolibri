dcac117178a9689f4092c363d91e893f552d09f5
Dierk Koenig <dierk.koenig@canoo.com>
2024-11-28 14:26:48 +0100

    navigation: move site projector from examples to kolibri navigation projectors

--- docs/src/examples/navigation/simple/starter.js
+++ docs/src/examples/navigation/simple/starter.js
@@ -3,8 +3,8 @@ import { defaultConsoleLogging }         from "../../../kolibri/logger/loggingSu
 import { HomePage  }                     from "./home.js";
 import { AboutPage }                     from "./about.js";
 import { URI_HASH_HOME, URI_HASH_ABOUT } from "../../../customize/uriHashes.js";
-import { SimpleNavigationProjector }     from "../../../kolibri/navigation/projector/simple/simpleNavigationProjector.js";
-import { SiteProjector }                 from "./siteProjector.js";
+import { SimpleNavigationProjector } from "../../../kolibri/navigation/projector/simple/simpleNavigationProjector.js";
+import { SiteProjector }             from "../../../kolibri/navigation/projector/siteProjector.js";
 
 // defaultConsoleLogging("ch.fhnw.kolibri", LOG_INFO);
 defaultConsoleLogging("ch.fhnw.kolibri.navigation", LOG_DEBUG);
