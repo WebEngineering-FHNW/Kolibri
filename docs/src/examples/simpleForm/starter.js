e26de7e62ed92f2cad259a234685015a95d4e778
Fabian Affolter <fabian.affolter@fhnw.ch>
2025-01-01 22:17:27 +0100

    Add Range Slider
    added the input range slider to simpleInput

--- docs/src/examples/simpleForm/starter.js
+++ docs/src/examples/simpleForm/starter.js
@@ -1,6 +1,7 @@
-import { projectForm, FORM_CSS }                     from "../../kolibri/projector/simpleForm/simpleFormProjector.js"
-import { SimpleFormController }                      from "../../kolibri/projector/simpleForm/simpleFormController.js"
-import { CHECKBOX, NUMBER, TEXT, COLOR, DATE, TIME } from "../../kolibri/util/dom.js";
+import { projectForm, FORM_CSS }                            from "../../kolibri/projector/simpleForm/simpleFormProjector.js"
+import { SimpleFormController }                             from "../../kolibri/projector/simpleForm/simpleFormController.js"
+import { CHECKBOX, NUMBER, TEXT, COLOR, DATE, TIME, RANGE } from "../../kolibri/util/dom.js";
+import { SIMPLE_INPUT_RANGE_SLIDER_CSS}                     from "../../kolibri/projector/simpleForm/simpleInputProjector.js";
 
 export { start } // exported for testing purposes
 
@@ -11,7 +12,8 @@ const start = () => {
         {value: "1968-04-19", label: "Date",   name: "date",   type: DATE     },
         {value: 12 * 60 + 15, label: "Time",   name: "time",   type: TIME     },
         {value: false,        label: "Check",  name: "check",  type: CHECKBOX },
-        {value: "",           label: "Color",  name: "color",  type: COLOR    }
+        {value: "",           label: "Color",  name: "color",  type: COLOR    },
+        {value: 42,           label: "Range",  name: "range",  type: RANGE  },
     ];
     const controller = SimpleFormController(formStructure);
     return projectForm(controller);
@@ -22,5 +24,6 @@ const start = () => {
 const formHolder = document.getElementById("form-holder");
 if (null != formHolder) { // there is no such element when called via test case
     document.querySelector("head style").textContent += FORM_CSS;
+    document.querySelector("head style").textContent += SIMPLE_INPUT_RANGE_SLIDER_CSS;
     formHolder.append(...start());
 }
